176ca0b3e6c0a769e98ff66924a8085715bf652e
Ahmed <38337765+AhmedTElthakeb@users.noreply.github.com>
2021-09-22 17:15:41 -0700

    update copyrights (#79)

--- mobile_back_samsung/cpp/eden_mlperf_c_backend/jni/eden_sdk/include/edenai.h
+++ mobile_back_samsung/cpp/eden_mlperf_c_backend/jni/eden_sdk/include/edenai.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 The MLPerf Authors. All Rights Reserved.
+/* Copyright (c) 2020-2021 Samsung Electronics Co., Ltd. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
