d0663163645471c0177c4e2479023370f454b708
fatihcakirs <69474183+fatihcakirs@users.noreply.github.com>
2021-11-08 17:15:31 -0800

    Update upscale_coco.py

--- datasets/util/coco/upscale_coco.py
+++ datasets/util/coco/upscale_coco.py
@@ -1,5 +1,4 @@
-# Copyright (c) 2020-2021 Qualcomm Innovation Center, Inc. All rights reserved.
-# Copyright (c) 2021 Samsung Electronics Co. Ltd. All rights reserved.
+# Copyright (c) 2020-2021 The MLPerf Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
