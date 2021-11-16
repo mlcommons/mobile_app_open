56082c27e40c664dfa73c07fa460202de34e5b94
Anh <anh.app.dev@gmail.com>
2021-11-16 11:55:18 +0100

    Change company name (#137)
    * Change com.xored to org.mlcommons
    * Update bundle ID and org name

--- flutter/android/app/src/main/kotlin/com/xored/mobile_app_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.xored.mobile_app_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ flutter/android/app/src/main/kotlin/org/mlcommons/mobile_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.mlcommons.android.mlperfbench
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
