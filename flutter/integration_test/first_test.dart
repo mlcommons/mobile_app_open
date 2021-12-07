b83441ff73ad20db7f2fd1c9ecf2799eb8002ad5
Danil Uzlov <DanilUzlov@yandex.ru>
2021-12-07 16:13:38 +0700

    fix linter
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- flutter/integration_test/first_test.dart
+++ flutter/integration_test/first_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:io';
 import 'dart:convert';
