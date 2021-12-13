bb882a5c234009bc502c05853b3e7306868567a2
Danil Uzlov <DanilUzlov@yandex.ru>
2021-12-13 16:23:10 +0700

    fix WITH_SAMSUNG
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- Makefile
+++ Makefile
@@ -27,7 +27,7 @@ include tools/common.mk
 include tools/formatter/format.mk
 
 include mobile_back_tflite/tflite_backend.mk
-include mobile_back_tflite/samsung_backend.mk
+include mobile_back_samsung/samsung_backend.mk
 include mobile_back_qti/make/qti_backend.mk
 include mobile_back_qti/make/qti_backend_targets.mk
 include mobile_back_pixel/pixel_backend.mk
--- mobile_back_samsung/samsung_backend.mk
+++ mobile_back_samsung/samsung_backend.mk
@@ -17,31 +17,31 @@ ifeq (${WITH_SAMSUNG},1)
   $(info WITH_SAMSUNG=1)
   ANDROID_SAMSUNG_BACKEND_BAZEL_FLAG=--//android/java/org/mlperf/inference:with_samsung="1"
 
-  ifeq (${MOBILE_BACK_SAMSUNG_ROOT},)
-    $(error MOBILE_BACK_SAMSUNG_ROOT env must be defined when building with samsung backend)
+  ifeq (${MOBILE_BACK_SAMSUNG_LIB_ROOT},)
+    $(error MOBILE_BACK_SAMSUNG_LIB_ROOT env must be defined when building with samsung backend)
   endif
-  backend_samsung_android_files=${MOBILE_BACK_SAMSUNG_ROOT}/lib/libsamsungbackend.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libeden_model.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libeden_profiler.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libeden_rt.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libeden_ud_gpu.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libeden_ud_cpu.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libeden_ud_npu.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libeden_nn.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libeden_osal.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libeden_xtool.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libc++.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libeden_nn_on_system.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libutils.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libcutils.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libvndksupport.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libbase.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libcgrouprc.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libhidlbase.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libgpu_boost_vendor.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libprocessgroup.so \
-    ${MOBILE_BACK_SAMSUNG_ROOT}/lib/libvendor_samsung_slsi_hardware_gpu_boost.so \
-  # Samsung backend is prebuilt, so we don't need any bazel target for it
+  backend_samsung_android_files=${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libsamsungbackend.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libeden_model.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libeden_profiler.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libeden_rt.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libeden_ud_gpu.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libeden_ud_cpu.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libeden_ud_npu.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libeden_nn.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libeden_osal.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libeden_xtool.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libc++.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libeden_nn_on_system.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libutils.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libcutils.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libvndksupport.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libbase.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libcgrouprc.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libhidlbase.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libgpu_boost_vendor.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libprocessgroup.so \
+    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libvendor_samsung_slsi_hardware_gpu_boost.so \
+  # Samsung backend is prebuilt, so we don't need any bazel targets for it
   backend_samsung_android_target=
   backend_samsung_filename=libsamsungbackend
 endif
