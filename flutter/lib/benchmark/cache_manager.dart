b287688daf5556f0d5b0cf0641500cb9049cd9f6
Danil Uzlov <DanilUzlov@yandex.ru>
2021-12-27 15:39:17 +0700

    fix cache purge on windows
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- flutter/lib/benchmark/cache_manager.dart
+++ flutter/lib/benchmark/cache_manager.dart
@@ -20,16 +20,19 @@ class CacheManager {
       [int atLeastDaysOld = 0]) async {
     final directory = Directory(loadedResourcesDir);
 
-    // can't use await for here because we delete files,
-    // and await for on stream from .list() throws exceptions when file is missing
+    // can't use 'await for' here because we delete files,
+    // and 'await for' on stream from directory.list() throws exceptions when file is missing
     for (final file in await directory.list(recursive: true).toList()) {
       // skip files in already deleted folders
       if (!await file.exists()) continue;
-      final relativePath = file.path.substring(loadedResourcesDir.length + 1);
+      final relativePath = file.path.substring(loadedResourcesDir.length + 1).replaceAll('\\', '/');
       var nonRemovable = false;
       for (var resource in nonRemovableResources) {
         // relativePath.startsWith(resource): if we want to preserve a folder resource
         // resource.startsWith(relativePath): if we want to preserve a file resource
+        //   for example:
+        //   we are checking folder 'github.com'
+        //   resource is 'github.com/mlcommons/mobile_models/raw/main/v0_7/datasets/ade20k'
         if (relativePath.startsWith(resource) ||
             resource.startsWith(relativePath)) {
           nonRemovable = true;
@@ -50,17 +53,16 @@ class CacheManager {
   Future<void> purgeOutdatedCache(int atLeastDaysOld) async {
     var currentResources = <String>[];
     for (var r in _resourcesMap.values) {
-      if (!r.startsWith(loadedResourcesDir)) continue;
       currentResources.add(r.substring(loadedResourcesDir.length + 1));
     }
     return deleteLoadedResources(currentResources, atLeastDaysOld);
   }
 
-  Future<void> cache(List<String> resources, void Function(double) reportProgress, bool purgeOldCache) async {
+  Future<void> cache(List<String> urls, void Function(double) reportProgress, bool purgeOldCache) async {
     final resourcesToDownload = <String>[];
     _resourcesMap = {};
 
-    for (final resource in resources) {
+    for (final resource in urls) {
       if (_resourcesMap.containsKey(resource)) continue;
       if (resourcesToDownload.contains(resource)) continue;
 
@@ -92,18 +94,17 @@ class CacheManager {
     return resource.endsWith(ArchiveCacheHelper.extension);
   }
 
-  Future<void> _download(List<String> resources, void Function(double) reportProgress) async {
+  Future<void> _download(List<String> urls, void Function(double) reportProgress) async {
     var progress = 0.0;
-    for (var url in resources) {
+    for (var url in urls) {
       if (isResourceAnArchive(url)) {
         _resourcesMap[url] = await archiveCacheHelper.get(url, true);
       } else {
         _resourcesMap[url] = await fileCacheHelper.get(url, true);
       }
 
-      progress += 1.0 / resources.length;
+      progress += 1.0 / urls.length;
       reportProgress(progress);
     }
   }
-
 }
