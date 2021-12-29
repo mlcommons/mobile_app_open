252f7308adc7bfd9a05c3e6abe766c5692ec055d
Danil Uzlov <DanilUzlov@yandex.ru>
2021-12-29 16:06:18 +0700

    refactor result manager
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- flutter/lib/resources/result_manager.dart
+++ flutter/lib/resources/result_manager.dart
@@ -5,6 +5,43 @@ import 'package:mlperfbench/benchmark/benchmark.dart';
 import 'package:mlperfbench/benchmark/benchmark_result.dart';
 import 'package:mlperfbench/protos/backend_setting.pb.dart' as pb;
 
+class _BriefResult {
+  static const _idTag = 'benchmark_id';
+  static const _scoreTag = 'score';
+  static const _accuracyTag = 'accuracy';
+  static const _shardsNumTag = 'shards_num';
+  static const _batchSizeTag = 'batch_size';
+
+  final String id;
+  final double? score;
+  final String? accuracy;
+  final int shardsNum;
+  final int batchSize;
+
+  _BriefResult(
+      this.id, this.score, this.accuracy, this.shardsNum, this.batchSize);
+
+  Map<String, dynamic> toJsonMap() {
+    return {
+      _idTag: id,
+      _scoreTag: score,
+      _accuracyTag: accuracy,
+      _shardsNumTag: shardsNum,
+      _batchSizeTag: batchSize,
+    };
+  }
+
+  static _BriefResult fromJsonMap(Map<String, dynamic> jsonMap) {
+    final id = jsonMap[_idTag] as String;
+    final score = jsonMap[_scoreTag] as double?;
+    final accuracy = jsonMap[_accuracyTag] as String?;
+    final shardsNum = jsonMap[_shardsNumTag] as int? ?? 0;
+    final batchSize = jsonMap[_batchSizeTag] as int? ?? 0;
+
+    return _BriefResult(id, score, accuracy, shardsNum, batchSize);
+  }
+}
+
 class ResultManager {
   static const _jsonResultFileName = 'result.json';
   late final String _jsonResultPath;
@@ -19,40 +56,32 @@ class ResultManager {
     final briefResultContent = <Map<String, dynamic>>[];
 
     for (final result in results) {
-      if (result != null) {
-        final score =
-            result.mode == BenchmarkMode.accuracy ? null : result.score;
+      if (result == null) continue;
+      final score = result.mode == BenchmarkMode.accuracy ? null : result.score;
 
-        final benchmarkResult = {
-          'benchmark_id': result.id,
-          'configuration': {
-            'runtime': '',
-          },
-          'score': score != null ? score.toString() : 'N/A',
-          'accuracy': BenchmarkMode.performance_lite == result.mode
-              ? 'N/A'
-              : result.accuracy,
-          // strings are used here to match android app behavior
-          'min_duration': result.minDuration.toString(),
-          'duration': result.durationMs.toString(),
-          'min_samples': result.minSamples.toString(),
-          'num_samples': result.numSamples.toString(),
-          'shards_num': result.threadsNumber,
-          'batch_size': result.batchSize,
-          'mode': result.mode.toString(),
-          'datetime': DateTime.now().toIso8601String(),
-        };
-        final benchmarkBriefResult = {
-          'benchmark_id': result.id,
-          'score': score,
-          'accuracy': result.accuracy,
-          'shards_num': result.threadsNumber,
-          'batch_size': result.batchSize,
-        };
-
-        resultContent.add(benchmarkResult);
-        briefResultContent.add(benchmarkBriefResult);
-      }
+      final benchmarkResult = {
+        'benchmark_id': result.id,
+        'configuration': {
+          'runtime': '',
+        },
+        'score': score != null ? score.toString() : 'N/A',
+        'accuracy': BenchmarkMode.performance_lite == result.mode
+            ? 'N/A'
+            : result.accuracy,
+        // strings are used here to match android app behavior
+        'min_duration': result.minDuration.toString(),
+        'duration': result.durationMs.toString(),
+        'min_samples': result.minSamples.toString(),
+        'num_samples': result.numSamples.toString(),
+        'shards_num': result.threadsNumber,
+        'batch_size': result.batchSize,
+        'mode': result.mode.toString(),
+        'datetime': DateTime.now().toIso8601String(),
+      };
+      var briefResult = _BriefResult(result.id, score, result.accuracy,
+          result.threadsNumber, result.batchSize);
+      resultContent.add(benchmarkResult);
+      briefResultContent.add(briefResult.toJsonMap());
     }
     await _write(resultContent);
     return JsonEncoder().convert(briefResultContent);
@@ -63,26 +92,23 @@ class ResultManager {
 
     try {
       for (final resultContent in jsonDecode(briefResultsJson)) {
-        final result = resultContent as Map<String, dynamic>;
-        final id = result['benchmark_id'] as String;
-        final accuracy = result['accuracy'] as String?;
-        final score = result['score'] as double?;
-        final threadsNumber = result['shards_num'] as int?;
-        final batchSize = result['batch_size'] as int?;
-        final benchmark =
-            benchmarks.singleWhere((benchmark) => benchmark.id == id);
-        benchmark.accuracy = accuracy;
-        benchmark.score = score;
+        var briefResult =
+            _BriefResult.fromJsonMap(resultContent as Map<String, dynamic>);
+        final benchmark = benchmarks
+            .singleWhere((benchmark) => benchmark.id == briefResult.id);
+        benchmark.accuracy = briefResult.accuracy;
+        benchmark.score = briefResult.score;
 
         if (benchmark.modelConfig.scenario == 'Offline') {
-          benchmark.benchmarkSetting.customSetting.add(
-              pb.CustomSetting(id: 'batch_size', value: batchSize.toString()));
           benchmark.benchmarkSetting.customSetting.add(pb.CustomSetting(
-              id: 'shards_num', value: threadsNumber.toString()));
+              id: 'batch_size', value: briefResult.batchSize.toString()));
+          benchmark.benchmarkSetting.customSetting.add(pb.CustomSetting(
+              id: 'shards_num', value: briefResult.shardsNum.toString()));
           benchmark.benchmarkSetting.writeToBuffer();
         }
       }
-    } catch (_) {
+    } catch (e) {
+      print("can't parse previous results: $e");
       return false;
     }
     return true;
