7394e643c8ecc6182fb7000951c26a7cc00625d5
Anh <anh.app.dev@gmail.com>
2022-06-09 07:38:31 +0700

    Update _defaultConfigUrl to use tasks_flutterapp_v2.pbtxt

--- flutter/lib/resources/config_manager.dart
+++ flutter/lib/resources/config_manager.dart
@@ -8,7 +8,7 @@ import 'utils.dart';
 const _configListFileName = 'benchmarksConfigurations.json';
 const _defaultConfigName = 'default';
 const _defaultConfigUrl =
-    'https://raw.githubusercontent.com/mlcommons/mobile_models/main/v2_0/assets/tasks_flutterapp.pbtxt';
+    'https://raw.githubusercontent.com/mlcommons/mobile_models/main/v2_0/assets/tasks_flutterapp_v2.pbtxt';
 
 class BenchmarksConfig {
   final String name;
