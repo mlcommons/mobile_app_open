9cca4d1235987c9b11d0b0bfaf5f9683f2f6bff6
Anh <anh.app.dev@gmail.com>
2022-06-15 17:00:50 +0700

    Define exact version for dependencies

--- flutter/ios/ci_scripts/ci_post_clone.sh
+++ flutter/ios/ci_scripts/ci_post_clone.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-echo "Navigate from ($PWD) to ($CI_WORKSPACE)"
-cd "$CI_WORKSPACE" || exit 1
+cd ../../.. || exit 1
+echo "Current working directory is $PWD"
+export ROOT_DIR=$PWD
+
+# check if we are in the root directory of the repo, where the WORKSPACE file exists
+test -f WORKSPACE || exit 1
 
 echo "Install dependencies"
-HOMEBREW_NO_AUTO_UPDATE=1 brew install bazelisk protobuf cocoapods
-python3 -m pip install --user numpy absl-py
+export HOMEBREW_NO_AUTO_UPDATE=1
+brew extract --version=1.11.0 bazelisk homebrew/cask && brew install bazelisk@1.11.0
+brew extract --version=3.19.4 protobuf homebrew/cask && brew install protobuf@3.19.4
+brew extract --version=1.11.3 cocoapods homebrew/cask && brew install cocoapods@1.11.3
+
+python3 -m pip install --user \
+  numpy==1.21.5 \
+  absl-py==1.0.0
 
 echo "Install Flutter"
 mkdir -p ~/tools && git clone --branch 2.10.5 --depth 1 https://github.com/flutter/flutter.git ~/tools/flutter
@@ -14,7 +24,7 @@ dart pub global activate protoc_plugin
 
 echo "Build app"
 make
-cd "$CI_WORKSPACE"/flutter || exit 1 && flutter precache --ios
-cd "$CI_WORKSPACE"/flutter/ios || exit 1 && pod install
+cd "$ROOT_DIR"/flutter || exit 1 && flutter precache --ios
+cd "$ROOT_DIR"/flutter/ios || exit 1 && pod install
 
 exit 0
