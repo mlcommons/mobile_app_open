16e63cf584558ff0c729e387846f89c1e57ca76b
Anh <anh.app.dev@gmail.com>
2022-06-15 18:15:06 +0700

    Update ci_post_clone.sh

--- flutter/ios/ci_scripts/ci_post_clone.sh
+++ flutter/ios/ci_scripts/ci_post_clone.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-cd ../../.. || exit 1
-echo "Current working directory is $PWD"
-export ROOT_DIR=$PWD
+# abort if an error occurred
+set -e
 
+cd ../../../
+echo "Current working directory is $PWD"
 # check if we are in the root directory of the repo, where the WORKSPACE file exists
-test -f WORKSPACE || exit 1
+test -f WORKSPACE
+export ROOT_DIR=$PWD
 
 echo "Install dependencies"
 export HOMEBREW_NO_AUTO_UPDATE=1
@@ -13,18 +15,22 @@ brew extract --version=1.11.0 bazelisk homebrew/cask && brew install bazelisk@1.
 brew extract --version=3.19.4 protobuf homebrew/cask && brew install protobuf@3.19.4
 brew extract --version=1.11.3 cocoapods homebrew/cask && brew install cocoapods@1.11.3
 
-python3 -m pip install --user \
+python3.9 --version
+python3.9 -m pip install \
   numpy==1.21.5 \
   absl-py==1.0.0
 
 echo "Install Flutter"
-mkdir -p ~/tools && git clone --branch 2.10.5 --depth 1 https://github.com/flutter/flutter.git ~/tools/flutter
-export PATH="$PATH:$HOME/tools/flutter/bin:$HOME/.pub-cache/bin"
+export BUILD_HOME="$HOME"/build
+export FLUTTER_HOME="$BUILD_HOME"/flutter
+mkdir -p "$BUILD_HOME"
+test ! -d "$FLUTTER_HOME" && git clone --branch 2.10.5 --depth 1 https://github.com/flutter/flutter.git "$FLUTTER_HOME"
+export PATH="$PATH:$FLUTTER_HOME/bin:$HOME/.pub-cache/bin"
 dart pub global activate protoc_plugin
 
 echo "Build app"
-make
-cd "$ROOT_DIR"/flutter || exit 1 && flutter precache --ios
-cd "$ROOT_DIR"/flutter/ios || exit 1 && pod install
+cd "$ROOT_DIR"/flutter && flutter precache --ios
+cd "$ROOT_DIR"/flutter/ios && pod install
+cd "$ROOT_DIR" && make
 
 exit 0
