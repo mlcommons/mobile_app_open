9f6e68e4feb4da6ac094071298ae4c9c154cb350
Anh <anh.app.dev@gmail.com>
2022-06-30 19:22:42 +0700

    Fix the issue where `make docker/format` fails at `format/ts`

--- tools/formatter/format.mk
+++ tools/formatter/format.mk
@@ -28,6 +28,7 @@ format/dart:
 
 .PHONY: format/ts
 format/ts:
+	cd firebase_functions/functions && ${_start_args} npm install --production=false
 	cd firebase_functions/functions && ${_start_args} npm run format
 	cd firebase_functions/functions && ${_start_args} npm run lint-fix
 
