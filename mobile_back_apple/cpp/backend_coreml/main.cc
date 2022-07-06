673681ef5333cde8845f2d5a78b0dc37b0f8252d
freedom" Koan-Sin Tan <koansin.tan@gmail.com>
2022-07-06 15:44:25 +0800

    proceeding to number of input & output tensors

--- mobile_back_apple/cpp/backend_coreml/main.cc
+++ mobile_back_apple/cpp/backend_coreml/main.cc
@@ -1,30 +1,45 @@
-#include <string.h>
+#include <cstring>
+
+#include "coreml_settings.h"
+#include "coreml_settings.h"
 
 #include "flutter/cpp/c/backend_c.h"
 #include "flutter/cpp/c/type.h"
 
+#import <CoreML/CoreML.h>
+
+struct CoreMLBackendData {
+  const char *name = "Core ML";
+  const char *vendor = "Apple";
+  const char *accelerator = "Neural Engine";
+  MLModel *model{nullptr};
+  uint32_t real_batch_size = 1;
+  int32_t original_tensor_size = 0;
+};
+
+static bool backendExists = false;
+
 // Return the name of the backend
 const char *mlperf_backend_vendor_name(mlperf_backend_ptr_t backend_ptr) {
-  return "Apple";
+  return ((CoreMLBackendData *)backend_ptr)->vendor;
 }
 
 // Return the name of the accelerator.
 const char *mlperf_backend_accelerator_name(mlperf_backend_ptr_t backend_ptr) {
-  return "Neural Engine";
+  return ((CoreMLBackendData *)backend_ptr)->accelerator;
 }
 
 // Return the name of this backend.
 const char *mlperf_backend_name(mlperf_backend_ptr_t backend_ptr) {
-  (void)backend_ptr;
-  return "Core ML";
+  return ((CoreMLBackendData *)backend_ptr)->name;
 }
 
 // Should return true if current hardware is supported.
 bool mlperf_backend_matches_hardware(const char **not_allowed_message,
                                      const char **settings,
                                      const mlperf_device_info_t *device_info) {
-  (void)settings;
   (void)device_info;
+  *settings = coreml_settings.c_str();
   *not_allowed_message = "This is a stub backend for Core ML";
   return true;
 }
@@ -33,7 +48,24 @@ bool mlperf_backend_matches_hardware(const char **not_allowed_message,
 mlperf_backend_ptr_t mlperf_backend_create(
     const char *model_path, mlperf_backend_configuration_t *configs,
     const char *native_lib_path) {
-  return nullptr;
+
+  // Verify only one instance of the backend exists at any time
+  if (backendExists) {
+    NSLog(@"Only one backend instance should exist at a time");
+    return nullptr;
+  }
+
+  CoreMLBackendData *backend_data = new CoreMLBackendData();
+
+  backendExists = true;
+
+  // Load the model.
+  NSURL *modelURL = [NSURL URLWithString: [NSString stringWithCString: model_path encoding: NSUTF8StringEncoding]];
+  NSURL *compiledModelURL = [MLModel compileModelAtURL: modelURL error: nil];
+  MLModel *mlmodel = [MLModel modelWithContentsOfURL: compiledModelURL error: nil];
+  backend_data->model = mlmodel;
+
+  return backend_data;
 }
 
 // Destroy the backend pointer and its data.
@@ -53,7 +85,8 @@ mlperf_status_t mlperf_backend_flush_queries(mlperf_backend_ptr_t backend_ptr) {
 
 // Return the number of inputs of the model.
 int32_t mlperf_backend_get_input_count(mlperf_backend_ptr_t backend_ptr) {
-  return -1;
+  NSLog(@"input %d", [[[((CoreMLBackendData *) backend_ptr)->model modelDescription] inputDescriptionsByName] count]);
+  return [[[((CoreMLBackendData *) backend_ptr)->model modelDescription] inputDescriptionsByName] count];
 }
 
 // Return the type of the ith input.
@@ -73,7 +106,8 @@ mlperf_status_t mlperf_backend_set_input(mlperf_backend_ptr_t backend_ptr,
 
 // Return the number of outputs for the model.
 int32_t mlperf_backend_get_output_count(mlperf_backend_ptr_t backend_ptr) {
-  return -1;
+  NSLog(@"output count %d", [[[((CoreMLBackendData *) backend_ptr)->model modelDescription] outputDescriptionsByName] count]);
+  return [[[((CoreMLBackendData *) backend_ptr)->model modelDescription] outputDescriptionsByName] count];
 }
 
 // Return the type of ith output.
