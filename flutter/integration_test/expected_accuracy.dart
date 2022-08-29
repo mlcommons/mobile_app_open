ae26de7cce900b31a15e742d21f957bb0a0ad165
Anh <anh.app.dev@gmail.com>
2022-08-29 21:07:21 +0700

    Update accuracy mapping for Core ML backend

--- flutter/integration_test/expected_accuracy.dart
+++ flutter/integration_test/expected_accuracy.dart
@@ -9,7 +9,7 @@ const Map<String, Interval> _imageClassification = {
   'CPU': Interval(min: 1.0, max: 1.0),
   'NNAPI': Interval(min: 0.8999, max: 0.9),
   'CoreML': Interval(min: 1.0, max: 1.0),
-  'Neural Engine': Interval(min: 1.0, max: 1.0),
+  'ANE': Interval(min: 1.0, max: 1.0),
 };
 
 const Map<String, Interval> _objectDetection = {
@@ -17,7 +17,7 @@ const Map<String, Interval> _objectDetection = {
   'NNAPI': Interval(min: 0.2816, max: 0.3034),
   'NNAPI+TFLite-pixel': Interval(min: 0.3609, max: 0.3658),
   'CoreML': Interval(min: 0.3162, max: 0.3163),
-  'Neural Engine': Interval(min: 0.4558, max: 0.4559),
+  'ANE': Interval(min: 0.4558, max: 0.4559),
 };
 
 const Map<String, Interval> _imageSegmentation = {
@@ -25,7 +25,7 @@ const Map<String, Interval> _imageSegmentation = {
   'NNAPI': Interval(min: 0.4836, max: 0.4872),
   'CoreML': Interval(min: 0.9887, max: 0.9888),
   // accuracy in emulator is 0.83, on real device 0.98
-  'Neural Engine': Interval(min: 0.8387, max: 0.9891),
+  'ANE': Interval(min: 0.8387, max: 0.9891),
 };
 
 const Map<String, Interval> _languageUnderstanding = {
@@ -33,7 +33,7 @@ const Map<String, Interval> _languageUnderstanding = {
   'GPU (FP16)': Interval(min: 1.0, max: 1.0),
   'NNAPI': Interval(min: 1.0, max: 1.0),
   'Metal': Interval(min: 1.0, max: 1.0),
-  'Neural Engine': Interval(min: 1.0, max: 1.0),
+  'ANE': Interval(min: 1.0, max: 1.0),
 };
 
 const benchmarkExpectedAccuracy = {
