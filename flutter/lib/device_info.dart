a705cf0e6b520d2013f08bf8f73722d993d209f7
Uzlov Danil <36223296+d-uzlov@users.noreply.github.com>
2022-09-14 10:14:36 +0700

    Refactor environment info generation (#510)
    * refactor DeviceInfo
    
    * refactor BackendInfo
    
    * fix manufacturer field for windows

--- flutter/lib/device_info.dart
+++ flutter/lib/device_info.dart
@@ -7,43 +7,79 @@ import 'package:mlperfbench_common/data/environment/environment_info.dart';
 import 'package:mlperfbench_common/data/environment/os_enum.dart';
 
 class DeviceInfo {
+  final String modelCode;
+  final String modelName;
+  final String manufacturer;
+
   static late final String nativeLibraryPath;
-  static late final String model;
-  static late final String manufacturer;
+  static late final DeviceInfo instance;
+
+  DeviceInfo({
+    required this.manufacturer,
+    required this.modelCode,
+    required this.modelName,
+  });
+
+  static Future<void> staticInit() async {
+    DeviceInfo.nativeLibraryPath = await _makeNativeLibraryPath();
+    DeviceInfo.instance = await createFromEnvironment();
+  }
+
+  static Future<DeviceInfo> createFromEnvironment() async {
+    if (Platform.isIOS) {
+      return _makeIosInfo();
+    } else if (Platform.isWindows) {
+      return _makeWindowsInfo();
+    } else if (Platform.isAndroid) {
+      return _makeAndroidInfo();
+    } else {
+      throw 'Could not define platform';
+    }
+  }
+
+  static Future<String> _makeNativeLibraryPath() async {
+    if (Platform.isAndroid) {
+      return await _getNativeLibraryPath();
+    } else {
+      return '';
+    }
+  }
+
+  static Future<DeviceInfo> _makeIosInfo() async {
+    final deviceInfo = DeviceInfoPlugin();
+    final iosInfo = await deviceInfo.iosInfo;
+
+    return DeviceInfo(
+      manufacturer: 'Apple',
+      modelCode: iosInfo.utsname.machine,
+      modelName: '',
+    );
+  }
+
+  static Future<DeviceInfo> _makeAndroidInfo() async {
+    final deviceInfo = await DeviceInfoPlugin().androidInfo;
+    return DeviceInfo(
+      manufacturer: deviceInfo.manufacturer,
+      modelCode: deviceInfo.model,
+      modelName: '',
+    );
+  }
+
+  static Future<DeviceInfo> _makeWindowsInfo() async {
+    return DeviceInfo(
+      manufacturer: '',
+      modelCode: 'Unknown PC',
+      modelName: '',
+    );
+  }
 
   static EnvironmentInfo get environmentInfo {
     return EnvironmentInfo(
       osName: OsName.fromJson(Platform.operatingSystem),
       osVersion: Platform.operatingSystemVersion,
-      manufacturer: DeviceInfo.manufacturer,
-      model: DeviceInfo.model,
+      manufacturer: DeviceInfo.instance.manufacturer,
+      model: DeviceInfo.instance.modelCode,
     );
-  }
-
-  static Future<void> staticInit() async {
-    if (Platform.isAndroid) {
-      DeviceInfo.nativeLibraryPath = await _getNativeLibraryPath();
-    } else {
-      DeviceInfo.nativeLibraryPath = '';
-    }
-
-    if (Platform.isIOS) {
-      final deviceInfo = DeviceInfoPlugin();
-      final iosInfo = await deviceInfo.iosInfo;
-
-      DeviceInfo.manufacturer = 'Apple';
-      DeviceInfo.model = iosInfo.utsname.machine;
-    } else if (Platform.isWindows) {
-      DeviceInfo.manufacturer = 'Microsoft';
-      DeviceInfo.model = 'Unknown PC';
-    } else if (Platform.isAndroid) {
-      final deviceInfo = await DeviceInfoPlugin().androidInfo;
-
-      DeviceInfo.manufacturer = deviceInfo.manufacturer;
-      DeviceInfo.model = deviceInfo.model;
-    } else {
-      throw 'Could not define platform';
-    }
   }
 }
 
