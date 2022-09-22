d82ffd692857dcc7afbf75aa5180b106f1e5779d
Anh <anh.app.dev@gmail.com>
2022-09-22 20:59:10 +0700

    Cache bazel

--- tools/scanner/scan.mk
+++ tools/scanner/scan.mk
@@ -23,7 +23,8 @@ ifdef SONAR_OUT_DIR
 		--env SONAR_TOKEN=${SONAR_TOKEN} \
 		--env PR_NUMBER=${PR_NUMBER} \
 		--env PR_BRANCH=${PR_BRANCH} \
-		--env PR_BASE=${PR_BASE}
+		--env PR_BASE=${PR_BASE} \
+		-v ${BAZEL_CACHE_PATH}:${BAZEL_CACHE_PATH}
 
 	# https://docs.sonarcloud.io/advanced-setup/languages/c-c-objective-c/#building-with-bazel
 	sonar_bazel_startup_options=--batch
