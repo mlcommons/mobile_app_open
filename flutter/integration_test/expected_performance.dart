2cffa19b5c3d731d706b4c0db4cea98e7db0b1a0
Danil Uzlov <DanilUzlov@yandex.ru>
2022-09-26 13:07:35 +0700

    add perf test

---
+++ flutter/integration_test/expected_performance.dart
@@ -0,0 +1,103 @@
+// All valid tags have the following format: os_type+model_code+backend_file
+// In this file there are tags that don't match this format.
+// Thet are only here as a reference and will not be used in a test
+// unless you manually edit the tag to match the format.
+
+/// Windows
+// cloudbuild n2-standard-4 machine
+const _tagWindowsCI = 'windows+Unknown PC+libtflitebackend.dll';
+const _tagWindowsRyzen5600 =
+    'windows+Unknown PC+libtflitebackend.dll+ryzen5600x';
+
+/// Android
+const _tagPixel5 = 'android+Pixel 5+libtflitebackend.so';
+const _tagPixel6 = 'android+Pixel 6+libtflitepixelbackend.so';
+// extremely unstable, actual results may vary from 2x lower to 2x higher
+const _tagSamsungS21PlusExynosTflite = 'android+SM-G996B+libtflitebackend.so';
+// extremely unstable, actual results may vary from 2x lower to 2x higher
+const _tagSamsungS0UltraExynosTflite = 'android+SM-N985F+libtflitebackend.so';
+
+/// iPhone
+const _tagIphone12miniTflite =
+    'ios+iPhone13,1+libtflitebackend.framework/libtflitebackend';
+const _tagIphone12miniCoreml =
+    'ios+iPhone13,1+libcoremlbackend.framework/libcoremlbackend';
+
+/// iOS Simulator
+const _tagIosSimMacincloudTflite =
+    'ios+x86_64+libtflitebackend.framework/libtflitebackend';
+const _tagIosSimMBP2019Tflite =
+    'ios+x86_64+libtflitebackend.framework/libtflitebackend+mbp2019';
+
+const Map<String, double> _imageClassification = {
+  _tagWindowsRyzen5600: 34,
+  _tagWindowsCI: 10,
+  _tagPixel5: 104,
+  _tagPixel6: 1015,
+  _tagSamsungS0UltraExynosTflite: 64,
+  _tagSamsungS21PlusExynosTflite: 68,
+  _tagIosSimMacincloudTflite: 4.5,
+  _tagIphone12miniTflite: 654,
+  _tagIphone12miniCoreml: 545,
+  _tagIosSimMBP2019Tflite: 23,
+};
+
+const Map<String, double> _objectDetection = {
+  _tagWindowsRyzen5600: 18,
+  _tagWindowsCI: 4.8,
+  _tagPixel5: 48,
+  _tagPixel6: 440,
+  _tagSamsungS0UltraExynosTflite: 33,
+  _tagSamsungS21PlusExynosTflite: 38,
+  _tagIosSimMacincloudTflite: 2.2,
+  _tagIphone12miniTflite: 259,
+  _tagIphone12miniCoreml: 305,
+  _tagIosSimMBP2019Tflite: 12.5,
+};
+
+const Map<String, double> _imageSegmentation = {
+  _tagWindowsRyzen5600: 6,
+  _tagWindowsCI: 1.9,
+  _tagPixel5: 30,
+  _tagPixel6: 160,
+  _tagSamsungS0UltraExynosTflite: 15,
+  _tagSamsungS21PlusExynosTflite: 14,
+  _tagIosSimMacincloudTflite: 0.85,
+  _tagIphone12miniTflite: 22,
+  _tagIphone12miniCoreml: 75,
+  _tagIosSimMBP2019Tflite: 4,
+};
+
+const Map<String, double> _naturalLanguageProcessing = {
+  _tagWindowsRyzen5600: 3,
+  _tagWindowsCI: 1,
+  _tagPixel5: 2.5,
+  _tagPixel6: 67,
+  _tagSamsungS0UltraExynosTflite: 1.5,
+  _tagSamsungS21PlusExynosTflite: 1.5,
+  _tagIosSimMacincloudTflite: 0.9,
+  _tagIphone12miniTflite: 12,
+  _tagIphone12miniCoreml: 81,
+  _tagIosSimMBP2019Tflite: 2.3,
+};
+
+const Map<String, double> _imageClassificationOffline = {
+  _tagWindowsRyzen5600: 52,
+  _tagWindowsCI: 12,
+  _tagPixel5: 152,
+  _tagPixel6: 1500,
+  _tagSamsungS0UltraExynosTflite: 89,
+  _tagSamsungS21PlusExynosTflite: 110,
+  _tagIosSimMacincloudTflite: 8,
+  _tagIphone12miniTflite: 1600,
+  _tagIphone12miniCoreml: 1250,
+  _tagIosSimMBP2019Tflite: 35,
+};
+
+const taskExpectedPerformance = {
+  'image_classification': _imageClassification,
+  'object_detection': _objectDetection,
+  'image_segmentation_v2': _imageSegmentation,
+  'natural_language_processing': _naturalLanguageProcessing,
+  'image_classification_offline': _imageClassificationOffline,
+};
--- flutter/integration_test/first_test.dart
+++ flutter/integration_test/first_test.dart
@@ -1,11 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:mlperfbench/store.dart';
+import 'package:mlperfbench_common/data/environment/environment_info.dart';
 import 'package:mlperfbench_common/data/extended_result.dart';
 import 'package:mlperfbench_common/data/results/benchmark_result.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'expected_accuracy.dart';
+import 'expected_performance.dart';
 import 'utils.dart';
 
 void main() {
@@ -14,6 +16,8 @@ void main() {
   SharedPreferences.setMockInitialValues({
     StoreConstants.testMode: true,
     StoreConstants.submissionMode: true,
+    StoreConstants.testMinDuration: 15,
+    StoreConstants.testCooldown: 10,
   });
 
   group('integration tests', () {
@@ -43,6 +47,7 @@ void checkTasks(ExtendedResult extendedResults) {
     expect(benchmarkResult.performance!.throughput, isNotNull);
 
     checkAccuracy(benchmarkResult);
+    checkPerformance(benchmarkResult, extendedResults.envInfo);
   }
 }
 
@@ -89,5 +94,49 @@ void checkAccuracy(BenchmarkExportResult benchmarkResult) {
     accuracyValue.normalized,
     lessThanOrEqualTo(expectedValue.max),
     reason: 'accuracy for $tag is too high',
+  );
+}
+
+void checkPerformance(
+  BenchmarkExportResult benchmarkResult,
+  EnvironmentInfo environmentInfo,
+) {
+  const allowedDeviation = 0.1;
+
+  final expectedMap = taskExpectedPerformance[benchmarkResult.benchmarkId];
+  expect(
+    expectedMap,
+    isNotNull,
+    reason:
+        'missing expected performance map for ${benchmarkResult.benchmarkId}',
+  );
+  expectedMap!;
+
+  final os = environmentInfo.osName.toString();
+  final model = environmentInfo.modelCode;
+  final backend = benchmarkResult.backendInfo.filename;
+  String tag = '$os+$model+$backend';
+  final expectedValue = expectedMap[tag] ?? expectedMap[os];
+  expect(
+    expectedValue,
+    isNotNull,
+    reason: 'missing expected performance for $tag',
+  );
+  expectedValue!;
+
+  final run = benchmarkResult.performance;
+  run!;
+
+  final value = run.throughput;
+  value!;
+  expect(
+    value,
+    greaterThanOrEqualTo(expectedValue * (1 - allowedDeviation)),
+    reason: 'performance for $tag is too low',
+  );
+  expect(
+    value,
+    lessThanOrEqualTo(expectedValue * (1 + allowedDeviation)),
+    reason: 'performance for $tag is too high',
   );
 }
