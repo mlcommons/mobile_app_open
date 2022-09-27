31622a56c8def2972e0eb3e406406b8a1172c6f3
Ahmed Taha ELthakeb <ahmed.t.althakeb@gmail.com>
2022-09-26 21:32:32 -0700

    mobile_back_samsung update

--- mobile_back_samsung/samsung_backend.mk
+++ mobile_back_samsung/samsung_backend.mk
@@ -23,8 +23,11 @@ ifeq (${WITH_SAMSUNG},1)
     ${BAZEL_LINKS_PREFIX}bin/flutter/android/commonlibs/lib_arm64/libc++_shared.so \
     ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libsamsungbackend.so \
     ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libenn_public_api_cpp.so \
-    ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libsbe2200_core.so \
+	${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libenn_extension.so \
     ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libsbe1200_core.so \
+	${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libsbe2100_core.so \
+	${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libsbe2200_core.so \
+	${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libsbe2300_core.so \
     ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libc++.so \
     ${MOBILE_BACK_SAMSUNG_LIB_ROOT}/libeden_nn_on_system.so \
   # main binaries for Samsung backend are prebuilt
