b8c37441fb513a5c3425af37560d0064c27ff445
Uzlov Danil <36223296+d-uzlov@users.noreply.github.com>
2022-10-03 12:08:35 +0700

    Refactor integration test (#539)
    * refactor integration test
    
    * refactor preferences store
    
    * chane lu to nlp in expected accuracy
    
    * print accuracy in log if it is invalid

---
+++ flutter/integration_test/utils.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mlperfbench/ui/root/main_screen.dart';
+import 'package:mlperfbench/ui/run/result_screen.dart';
+import 'package:mlperfbench_common/data/extended_result.dart';
+import 'package:mlperfbench/resources/result_manager.dart' as result_manager;
+import 'package:mlperfbench/resources/resource_manager.dart'
+    as resource_manager;
+import 'package:mlperfbench/main.dart' as app;
+
+Future<void> runBenchmark(WidgetTester tester) async {
+  const splashPauseSeconds = 4;
+  const runTimeLimitMinutes = 20;
+  const downloadTimeLimitMinutes = 10;
+
+  await app.main();
+  await tester.pumpAndSettle(const Duration(seconds: splashPauseSeconds));
+
+  var goButtonIsPresented = await waitFor(
+      tester, downloadTimeLimitMinutes, const Key(MainKeys.goButton));
+
+  expect(goButtonIsPresented, true,
+      reason: 'Problems with downloading of datasets or models');
+  final goButton = find.byKey(const Key(MainKeys.goButton));
+  await tester.tap(goButton);
+
+  var scrollButtonIsPresented = await waitFor(
+      tester, runTimeLimitMinutes, const Key(ResultKeys.scrollResultsButton));
+
+  expect(scrollButtonIsPresented, true, reason: 'Test results were not found');
+}
+
+Future<ExtendedResult> obtainResult() async {
+  final applicationDirectory =
+      await resource_manager.ResourceManager.getApplicationDirectory();
+
+  final rm = result_manager.ResultManager(applicationDirectory);
+  await rm.init();
+
+  return rm.getLastResult();
+}
+
+Future<bool> waitFor(WidgetTester tester, int timeLimitMinutes, Key key) async {
+  var element = false;
+
+  for (var counter = 0;
+      counter < timeLimitMinutes * Duration.secondsPerMinute;
+      counter++) {
+    await tester.pumpAndSettle(const Duration(seconds: 1));
+    final searchResult = find.byKey(key);
+
+    if (tester.any(searchResult)) {
+      element = true;
+      break;
+    }
+  }
+
+  return element;
+}
+
+void printResults(ExtendedResult extendedResults) {
+  print('benchmark result json:');
+  for (final line in const JsonEncoder.withIndent('  ')
+      .convert(extendedResults)
+      .split('\n')) {
+    print(line);
+  }
+}
