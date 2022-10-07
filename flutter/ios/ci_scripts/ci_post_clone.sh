72abc4ba74d02fdafbdc11ea2831e1964736a03c
Anh <anh.app.dev@gmail.com>
2022-10-07 13:23:38 +0700

    Add missing BAZEL_CACHE_ARG to make target flutter/ios/libs

--- flutter/ios/ci_scripts/ci_post_clone.sh
+++ flutter/ios/ci_scripts/ci_post_clone.sh
@@ -23,6 +23,8 @@ if [ "$CI" = "TRUE" ]; then
   echo "$MC_LOG_PREFIX Running on CI machine"
   export MC_BUILD_HOME=$CI_DERIVED_DATA_PATH/mobile_app_open_build
   CACHE_BUCKET=xcodecloud-bazel-ios---mobile-app-build-290400
+  # Required that a secret env existed in the XCode Cloud workflow
+  # with key=GC_CREDS and value=<output of cmd `base64 mobile-app-build-290400-1b26aafa8afd.json`>
   GC_CREDS_FILE=$CI_WORKSPACE/mobile-app-build-290400-1b26aafa8afd.json
   echo "$GC_CREDS" | base64 --decode > "$GC_CREDS_FILE"
   export BAZEL_CACHE_ARG="--remote_cache=https://storage.googleapis.com/$CACHE_BUCKET --google_credentials=$GC_CREDS_FILE"
