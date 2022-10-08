c01736a27b21041d48a5dded2847d80d36cf249c
Anh <anh.app.dev@gmail.com>
2022-10-08 21:35:14 +0700

    Run iOS test on GitHub Actions

--- flutter/ios/ci_scripts/ci_post_clone.sh
+++ flutter/ios/ci_scripts/ci_post_clone.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-# This script runs after the repo is cloned and before any action execution.
+# This script runs after the repo is cloned and before any action execution in Xcode Cloud
 # Environment variables with `CI_` prefix are set by Xcode Cloud
 # (see https://developer.apple.com/documentation/xcode/environment-variable-reference)
+# Environment variables with `GITHUB_` prefix are set by GitHub Actions
+# (see https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables)
 # To avoid collision with other environment variables, we use `MC_` (MLCommons) prefix for our own variables.
 # This script can also be run on the local Mac machine for testing and debugging.
-# We use [ "$CI" = "TRUE" ] to test if the script is run on local machine or Xcode Cloud.
 
 # abort if an error occurred, show cmd executed
 set -ex
@@ -19,20 +20,34 @@ echo "$MC_LOG_PREFIX Current working directory is $PWD"
 test -f WORKSPACE
 export MC_REPO_HOME=$PWD
 
-if [ "$CI" = "TRUE" ]; then
-  echo "$MC_LOG_PREFIX Running on CI machine"
+XCODE_CLOUD="xcode-cloud"
+GITHUB_ACTIONS="github-actions"
+LOCAL="local"
+runner=""
+
+if [ -n "$CI_DERIVED_DATA_PATH" ]; then
+  echo "$MC_LOG_PREFIX Running on XCode Cloud"
   export MC_BUILD_HOME=$CI_DERIVED_DATA_PATH/mobile_app_open_build
   CACHE_BUCKET=xcodecloud-bazel-ios---mobile-app-build-290400
   # Required that a secret env existed in the XCode Cloud workflow
   # with key=GC_CREDS and value=<output of cmd `base64 mobile-app-build-290400-1b26aafa8afd.json`>
   GC_CREDS_FILE=$CI_WORKSPACE/mobile-app-build-290400-1b26aafa8afd.json
-  echo "$GC_CREDS" | base64 --decode > "$GC_CREDS_FILE"
+  echo "$GC_CREDS" | base64 --decode >"$GC_CREDS_FILE"
   export BAZEL_CACHE_ARG="--remote_cache=https://storage.googleapis.com/$CACHE_BUCKET --google_credentials=$GC_CREDS_FILE"
+  runner=$XCODE_CLOUD
+elif [ -n "$GITHUB_WORKSPACE" ]; then
+  echo "$MC_LOG_PREFIX Running on GitHub Actions"
+  export MC_BUILD_HOME=$GITHUB_WORKSPACE/mobile_app_open_build
+  export BAZEL_CACHE_ARG="--disk_cache=$BAZEL_CACHE_PATH"
+  runner=$GITHUB_ACTIONS
 else
   echo "$MC_LOG_PREFIX Running on local machine"
   export MC_BUILD_HOME=$HOME/mobile_app_open_build
+  runner=$LOCAL
 fi
+
 echo "$MC_LOG_PREFIX MC_BUILD_HOME is ${MC_BUILD_HOME}"
+echo "$MC_LOG_PREFIX runner is ${runner}"
 
 echo "$MC_LOG_PREFIX ========== Install dependencies =========="
 
@@ -55,6 +70,25 @@ pip3 install \
   numpy==1.21.5 \
   absl-py==1.0.0
 
+if [ $runner = $GITHUB_ACTIONS ]; then
+  echo "$MC_LOG_PREFIX ========== Install Android SDK =========="
+  export ANDROID_HOME=$HOME/Library/Android/sdk
+  mkdir -p $ANDROID_HOME/cmdline-tools
+  # sdkmanager expects to be placed into `$ANDROID_HOME/cmdline-tools/tools`
+  curl -L https://dl.google.com/android/repository/commandlinetools-mac-7583922_latest.zip | jar x &&
+    mv cmdline-tools $ANDROID_HOME/cmdline-tools/tools &&
+    chmod -R +x $ANDROID_HOME/cmdline-tools/tools/bin
+  export PATH=$PATH:$ANDROID_HOME/cmdline-tools/tools/bin
+  yes | sdkmanager --licenses >/dev/null
+  yes | sdkmanager \
+    "platform-tools" \
+    "build-tools;30.0.3" \
+    "platforms;android-29" \
+    "platforms;android-31" \
+    "ndk;21.4.7075529"
+  export ANDROID_NDK_HOME=$ANDROID_HOME/ndk/21.4.7075529
+fi
+
 echo "$MC_LOG_PREFIX ========== Install Flutter =========="
 export MC_FLUTTER_HOME=$MC_BUILD_HOME/flutter
 export PUB_CACHE=$MC_BUILD_HOME/.pub-cache
@@ -76,13 +110,20 @@ echo "$MC_LOG_PREFIX Build backend and Flutter packages"
 # Remember to update the next line if make commands are changed.
 cd "$MC_REPO_HOME" && time make flutter/prepare && time make flutter/ios
 
-if [ "$CI_XCODEBUILD_ACTION" = "build-for-testing" ]; then
-  cd "$MC_REPO_HOME"/flutter && flutter build ios --config-only integration_test/first_test.dart
-else
-  cd "$MC_REPO_HOME"/flutter && flutter build ios --config-only --dart-define=official-build="$OFFICIAL_BUILD"
+if [ $runner = $XCODE_CLOUD ]; then
+  if [ "$CI_XCODEBUILD_ACTION" = "build-for-testing" ]; then
+    cd "$MC_REPO_HOME"/flutter && flutter build ios --config-only integration_test/first_test.dart
+  else
+    cd "$MC_REPO_HOME"/flutter && flutter build ios --config-only --dart-define=official-build="$OFFICIAL_BUILD"
+  fi
 fi
 
 cd "$MC_REPO_HOME"/flutter/ios && pod install
+
+if [ $runner = $GITHUB_ACTIONS ]; then
+  open -a Simulator
+  cd "$MC_REPO_HOME" && make flutter/test
+fi
 
 echo "$MC_LOG_PREFIX END ci_post_clone"
 exit 0
