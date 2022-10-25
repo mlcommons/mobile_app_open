387345b9983cd7453eb806af75839faab2056e32
Danil Uzlov <DanilUzlov@yandex.ru>
2022-10-25 10:27:26 +0700

    update windows ci perf

--- flutter/integration_test/expected_performance.dart
+++ flutter/integration_test/expected_performance.dart
@@ -4,8 +4,8 @@ const _tagCoreml = 'libcoremlbackend';
 
 /// Windows
 // cloudbuild n2-standard-4 machine
-const _tagCloudBuildN2S4 = 'Intel Xeon 2.80GHz';
-// not a valid device name, it's only here as a reference
+const _tagCloudBuildX28 = 'Intel Xeon 2.80GHz';
+const _tagCloudBuildX23 = 'Intel Xeon 2.30GHz';
 const _tagRyzen5600 = 'AMD Ryzen 5 5600X 6-Core';
 
 const _tagPixel5 = 'Pixel 5';
@@ -15,7 +15,8 @@ const _tagIphone12mini = 'iPhone13,1';
 const _tagIosSimMBP2019 = 'x86_64';
 
 const Map<String, Map<String, double>> _imageClassification = {
-  _tagCloudBuildN2S4: {_tagTflite: 10},
+  _tagCloudBuildX23: {_tagTflite: 9.0},
+  _tagCloudBuildX28: {_tagTflite: 10},
   _tagRyzen5600: {_tagTflite: 34},
   _tagPixel5: {_tagTflite: 104},
   _tagPixel6: {_tagPixelSo: 1015},
@@ -27,7 +28,8 @@ const Map<String, Map<String, double>> _imageClassification = {
 };
 
 const Map<String, Map<String, double>> _objectDetection = {
-  _tagCloudBuildN2S4: {_tagTflite: 5.1},
+  _tagCloudBuildX23: {_tagTflite: 5.2},
+  _tagCloudBuildX28: {_tagTflite: 5.1},
   _tagRyzen5600: {_tagTflite: 18},
   _tagPixel5: {_tagTflite: 48},
   _tagPixel6: {_tagPixelSo: 440},
@@ -39,7 +41,8 @@ const Map<String, Map<String, double>> _objectDetection = {
 };
 
 const Map<String, Map<String, double>> _imageSegmentation = {
-  _tagCloudBuildN2S4: {_tagTflite: 1.9},
+  _tagCloudBuildX23: {_tagTflite: 2.0},
+  _tagCloudBuildX28: {_tagTflite: 1.9},
   _tagRyzen5600: {_tagTflite: 6},
   _tagPixel5: {_tagTflite: 30},
   _tagPixel6: {_tagPixelSo: 160},
@@ -51,7 +54,8 @@ const Map<String, Map<String, double>> _imageSegmentation = {
 };
 
 const Map<String, Map<String, double>> _naturalLanguageProcessing = {
-  _tagCloudBuildN2S4: {_tagTflite: 1},
+  _tagCloudBuildX23: {_tagTflite: 0.85},
+  _tagCloudBuildX28: {_tagTflite: 1},
   _tagRyzen5600: {_tagTflite: 3},
   _tagPixel5: {_tagTflite: 2.4},
   _tagPixel6: {_tagPixelSo: 67},
@@ -63,7 +67,8 @@ const Map<String, Map<String, double>> _naturalLanguageProcessing = {
 };
 
 const Map<String, Map<String, double>> _imageClassificationOffline = {
-  _tagCloudBuildN2S4: {_tagTflite: 12},
+  _tagCloudBuildX23: {_tagTflite: 11.4},
+  _tagCloudBuildX28: {_tagTflite: 12},
   _tagRyzen5600: {_tagTflite: 52},
   _tagPixel5: {_tagTflite: 152},
   _tagPixel6: {_tagPixelSo: 1550},
@@ -83,16 +88,9 @@ const taskExpectedPerformance = {
 };
 
 const expectedInstabilityMap = {
-  _tagCloudBuildN2S4: {
-    _tagTflite: 1.15,
-  },
-  _tagRyzen5600: {
-    _tagTflite: 1.05,
-  },
-  _tagPixel5: {
-    _tagTflite: 1.15,
-  },
-  _tagPixel6: {
-    _tagPixelSo: 1.1,
-  },
+  _tagCloudBuildX23: {_tagTflite: 1.1},
+  _tagCloudBuildX28: {_tagTflite: 1.1},
+  _tagRyzen5600: {_tagTflite: 1.05},
+  _tagPixel5: {_tagTflite: 1.15},
+  _tagPixel6: {_tagPixelSo: 1.1},
 };
