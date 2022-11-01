95f3556dd6efb8329d3e75b3962a73b0c2389651
Anh <anh.app.dev@gmail.com>
2022-11-02 00:39:13 +0700

    Update expected throughput values

--- flutter/integration_test/expected_throughput.dart
+++ flutter/integration_test/expected_throughput.dart
@@ -16,7 +16,7 @@ const _tagPixel6 = 'Pixel 6';
 
 // iOS
 const _tagIphoneOnGitHubAction = 'iPhone15,3';
-const _tagIosSimMBP2019 = 'x86_64';
+const _tagIphoneOnMacbookM1 = 'iPhone14,7';
 
 const Map<String, Map<String, Interval>> _imageClassification = {
   _tagTfliteBackend: {
@@ -25,10 +25,10 @@ const Map<String, Map<String, Interval>> _imageClassification = {
     _tagRyzen5600: Interval(min: 31, max: 37),
     _tagPixel5: Interval(min: 80, max: 120),
     _tagIphoneOnGitHubAction: Interval(min: 550, max: 750),
-    _tagIosSimMBP2019: Interval(min: 19, max: 27),
+    _tagIphoneOnMacbookM1: Interval(min: 19, max: 27),
   },
   _tagCoremlBackend: {
-    _tagIphoneOnGitHubAction: Interval(min: 450, max: 650),
+    _tagIphoneOnGitHubAction: Interval(min: 2, max: 7),
   },
   _tagPixelBackend: {
     _tagPixel6: Interval(min: 900, max: 1100),
@@ -42,10 +42,10 @@ const Map<String, Map<String, Interval>> _objectDetection = {
     _tagRyzen5600: Interval(min: 14, max: 22),
     _tagPixel5: Interval(min: 40, max: 55),
     _tagIphoneOnGitHubAction: Interval(min: 200, max: 300),
-    _tagIosSimMBP2019: Interval(min: 9, max: 16),
+    _tagIphoneOnMacbookM1: Interval(min: 9, max: 16),
   },
   _tagCoremlBackend: {
-    _tagIphoneOnGitHubAction: Interval(min: 250, max: 350),
+    _tagIphoneOnGitHubAction: Interval(min: 2, max: 7),
   },
   _tagPixelBackend: {
     _tagPixel6: Interval(min: 380, max: 490),
@@ -59,10 +59,10 @@ const Map<String, Map<String, Interval>> _imageSegmentation = {
     _tagRyzen5600: Interval(min: 5, max: 7),
     _tagPixel5: Interval(min: 25, max: 35),
     _tagIphoneOnGitHubAction: Interval(min: 18, max: 26),
-    _tagIosSimMBP2019: Interval(min: 3, max: 5),
+    _tagIphoneOnMacbookM1: Interval(min: 3, max: 6),
   },
   _tagCoremlBackend: {
-    _tagIphoneOnGitHubAction: Interval(min: 60, max: 90),
+    _tagIphoneOnGitHubAction: Interval(min: 0.5, max: 2),
   },
   _tagPixelBackend: {
     _tagPixel6: Interval(min: 140, max: 180),
@@ -76,10 +76,10 @@ const Map<String, Map<String, Interval>> _naturalLanguageProcessing = {
     _tagRyzen5600: Interval(min: 2.8, max: 3.2),
     _tagPixel5: Interval(min: 2.3, max: 2.7),
     _tagIphoneOnGitHubAction: Interval(min: 9, max: 15),
-    _tagIosSimMBP2019: Interval(min: 1.8, max: 3),
+    _tagIphoneOnMacbookM1: Interval(min: 1.8, max: 3),
   },
   _tagCoremlBackend: {
-    _tagIphoneOnGitHubAction: Interval(min: 65, max: 95),
+    _tagIphoneOnGitHubAction: Interval(min: 0.4, max: 1),
   },
   _tagPixelBackend: {
     _tagPixel6: Interval(min: 60, max: 75),
@@ -93,10 +93,10 @@ const Map<String, Map<String, Interval>> _imageClassificationOffline = {
     _tagRyzen5600: Interval(min: 45, max: 60),
     _tagPixel5: Interval(min: 120, max: 180),
     _tagIphoneOnGitHubAction: Interval(min: 1300, max: 1900),
-    _tagIosSimMBP2019: Interval(min: 30, max: 40),
+    _tagIphoneOnMacbookM1: Interval(min: 30, max: 45),
   },
   _tagCoremlBackend: {
-    _tagIphoneOnGitHubAction: Interval(min: 1000, max: 1500),
+    _tagIphoneOnGitHubAction: Interval(min: 3, max: 15),
   },
   _tagPixelBackend: {
     _tagPixel6: Interval(min: 1400, max: 1700),
