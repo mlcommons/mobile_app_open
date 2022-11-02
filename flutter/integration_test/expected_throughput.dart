6669ddb4bdb3969faa518f9e9e70af6c8f225846
Anh <anh.app.dev@gmail.com>
2022-11-02 07:43:17 +0700

    Rename prefix `tag` to `k`

--- flutter/integration_test/expected_throughput.dart
+++ flutter/integration_test/expected_throughput.dart
@@ -1,8 +1,8 @@
 import 'utils.dart';
 
-const _tagTfliteBackend = 'libtflitebackend';
-const _tagPixelBackend = 'libtflitepixelbackend';
-const _tagCoremlBackend = 'libcoremlbackend';
+const _kTFLiteBackend = 'libtflitebackend';
+const _kPixelBackend = 'libtflitepixelbackend';
+const _kCoreMLBackend = 'libcoremlbackend';
 
 // Windows
 // Google Cloud Build n2-standard-4 machine
@@ -19,7 +19,7 @@ const _tagIphoneOnGitHubAction = 'iPhone15,3';
 const _tagIphoneOnMacbookM1 = 'iPhone14,7';
 
 const Map<String, Map<String, Interval>> _imageClassification = {
-  _tagTfliteBackend: {
+  _kTFLiteBackend: {
     _tagCloudBuildX23: Interval(min: 8, max: 11),
     _tagCloudBuildX28: Interval(min: 8, max: 12),
     _tagRyzen5600: Interval(min: 31, max: 37),
@@ -27,16 +27,16 @@ const Map<String, Map<String, Interval>> _imageClassification = {
     _tagIphoneOnGitHubAction: Interval(min: 550, max: 750),
     _tagIphoneOnMacbookM1: Interval(min: 19, max: 27),
   },
-  _tagCoremlBackend: {
+  _kCoreMLBackend: {
     _tagIphoneOnGitHubAction: Interval(min: 2, max: 7),
   },
-  _tagPixelBackend: {
+  _kPixelBackend: {
     _tagPixel6: Interval(min: 900, max: 1100),
   },
 };
 
 const Map<String, Map<String, Interval>> _objectDetection = {
-  _tagTfliteBackend: {
+  _kTFLiteBackend: {
     _tagCloudBuildX23: Interval(min: 4, max: 7),
     _tagCloudBuildX28: Interval(min: 3.5, max: 7),
     _tagRyzen5600: Interval(min: 14, max: 22),
@@ -44,16 +44,16 @@ const Map<String, Map<String, Interval>> _objectDetection = {
     _tagIphoneOnGitHubAction: Interval(min: 200, max: 300),
     _tagIphoneOnMacbookM1: Interval(min: 9, max: 16),
   },
-  _tagCoremlBackend: {
+  _kCoreMLBackend: {
     _tagIphoneOnGitHubAction: Interval(min: 2, max: 7),
   },
-  _tagPixelBackend: {
+  _kPixelBackend: {
     _tagPixel6: Interval(min: 380, max: 490),
   },
 };
 
 const Map<String, Map<String, Interval>> _imageSegmentation = {
-  _tagTfliteBackend: {
+  _kTFLiteBackend: {
     _tagCloudBuildX23: Interval(min: 1, max: 3),
     _tagCloudBuildX28: Interval(min: 0.5, max: 3),
     _tagRyzen5600: Interval(min: 5, max: 7),
@@ -61,16 +61,16 @@ const Map<String, Map<String, Interval>> _imageSegmentation = {
     _tagIphoneOnGitHubAction: Interval(min: 18, max: 26),
     _tagIphoneOnMacbookM1: Interval(min: 3, max: 6),
   },
-  _tagCoremlBackend: {
+  _kCoreMLBackend: {
     _tagIphoneOnGitHubAction: Interval(min: 0.5, max: 2),
   },
-  _tagPixelBackend: {
+  _kPixelBackend: {
     _tagPixel6: Interval(min: 140, max: 180),
   },
 };
 
 const Map<String, Map<String, Interval>> _naturalLanguageProcessing = {
-  _tagTfliteBackend: {
+  _kTFLiteBackend: {
     _tagCloudBuildX23: Interval(min: 0.7, max: 1.1),
     _tagCloudBuildX28: Interval(min: 0.5, max: 1.1),
     _tagRyzen5600: Interval(min: 2.8, max: 3.2),
@@ -78,16 +78,16 @@ const Map<String, Map<String, Interval>> _naturalLanguageProcessing = {
     _tagIphoneOnGitHubAction: Interval(min: 9, max: 15),
     _tagIphoneOnMacbookM1: Interval(min: 1.8, max: 3),
   },
-  _tagCoremlBackend: {
+  _kCoreMLBackend: {
     _tagIphoneOnGitHubAction: Interval(min: 0.4, max: 1),
   },
-  _tagPixelBackend: {
+  _kPixelBackend: {
     _tagPixel6: Interval(min: 60, max: 75),
   },
 };
 
 const Map<String, Map<String, Interval>> _imageClassificationOffline = {
-  _tagTfliteBackend: {
+  _kTFLiteBackend: {
     _tagCloudBuildX23: Interval(min: 10, max: 14),
     _tagCloudBuildX28: Interval(min: 9, max: 14),
     _tagRyzen5600: Interval(min: 45, max: 60),
@@ -95,10 +95,10 @@ const Map<String, Map<String, Interval>> _imageClassificationOffline = {
     _tagIphoneOnGitHubAction: Interval(min: 1300, max: 1900),
     _tagIphoneOnMacbookM1: Interval(min: 30, max: 45),
   },
-  _tagCoremlBackend: {
+  _kCoreMLBackend: {
     _tagIphoneOnGitHubAction: Interval(min: 3, max: 15),
   },
-  _tagPixelBackend: {
+  _kPixelBackend: {
     _tagPixel6: Interval(min: 1400, max: 1700),
   },
 };
