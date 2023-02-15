7c4557525538ca4f2ffb1b2f7309b1698e70174d
Anh <anh.app.dev@gmail.com>
2023-02-15 18:18:25 +0700

    Update docs for ProcessOutput()

--- flutter/cpp/dataset.h
+++ flutter/cpp/dataset.h
@@ -53,9 +53,8 @@ class Dataset : public ::mlperf::QuerySampleLibrary {
   // GetData returns the data of a specific input.
   virtual std::vector<void*> GetData(int sample_idx) = 0;
 
-  // ProcessOutput processes the output data before sending to mlperf. This
-  // function only get called on Accuracy mode so we don't need to care about
-  // its performance. The argument sample_idx should only be used to help the
+  // ProcessOutput processes the output data before sending to mlperf.
+  // The argument sample_idx should only be used to help the
   // Dataset calculating the accuracy; it should not affect the returned
   // result by any means.
   virtual std::vector<uint8_t> ProcessOutput(
