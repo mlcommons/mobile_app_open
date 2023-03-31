4188e8fbbd32b22f26234d88cf9fe60c7a1111e4
Mohit Mundhra <quic_mmundhra@quicinc.com>
2023-03-31 18:15:38 +0530

    Fixing bazel lint errors

--- tools/formatter/format.mk
+++ tools/formatter/format.mk
@@ -5,7 +5,7 @@ format: format/bazel format/clang format/dart format/ts format/line-endings form
 
 .PHONY: format/bazel
 format/bazel:
-	buildifier -r .
+	buildifier -lint=warn -mode=check -r .
 
 .PHONY: format/clang
 format/clang:
