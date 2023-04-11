11fca28f6359b91a76aa43f8f6d73b20a2466ebe
Anh <anh.app.dev@gmail.com>
2023-04-11 17:15:10 +0700

    Remove use of free and malloc

--- mobile_back_tflite/cpp/backend_tflite/tflite_c.cc
+++ mobile_back_tflite/cpp/backend_tflite/tflite_c.cc
@@ -575,6 +575,6 @@ mlperf_status_t mlperf_backend_get_output(mlperf_backend_ptr_t backend_ptr,
   return MLPERF_SUCCESS;
 }
 
-void *mlperf_backend_get_buffer(size_t n) { return malloc(n); }
+void *mlperf_backend_get_buffer(size_t n) { return ::operator new(n); }
 
-void mlperf_backend_release_buffer(void *p) { free(p); }
+void mlperf_backend_release_buffer(void *p) { ::operator delete(p); }
