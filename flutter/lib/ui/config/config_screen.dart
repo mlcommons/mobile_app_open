8733b942341d0848a48bfb213015393071361fd4
Anh <anh.app.dev@gmail.com>
2023-05-09 19:36:25 +0700

    Format Dart

--- flutter/lib/ui/config/config_screen.dart
+++ flutter/lib/ui/config/config_screen.dart
@@ -1,6 +1,6 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 
+import 'package:collection/collection.dart';
 import 'package:provider/provider.dart';
 
 import 'package:mlperfbench/benchmark/benchmark.dart';
