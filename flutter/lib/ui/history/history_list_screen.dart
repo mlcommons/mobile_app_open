f61cfadeb805e0257afe0115d38a4e6dda474531
Anh <anh.app.dev@gmail.com>
2024-01-09 11:22:13 +0700

    Add icon to sort menu

--- flutter/lib/ui/history/history_list_screen.dart
+++ flutter/lib/ui/history/history_list_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 
 import 'package:mlperfbench/benchmark/state.dart';
 import 'package:mlperfbench/data/benchmarks_data_provider.dart';
+import 'package:mlperfbench/data/result_filter.dart';
 import 'package:mlperfbench/data/result_sort.dart';
 import 'package:mlperfbench/data/results/benchmark_result.dart';
 import 'package:mlperfbench/localizations/app_localizations.dart';
@@ -20,10 +21,12 @@ class HistoryListScreen extends StatefulWidget {
 }
 
 class _HistoryListScreenState extends State<HistoryListScreen> {
+  late AppLocalizations l10n;
+
   @override
   Widget build(BuildContext context) {
     final state = context.watch<BenchmarkState>();
-    final l10n = AppLocalizations.of(context);
+    l10n = AppLocalizations.of(context);
     final localResults = state.resourceManager.resultManager.localResults;
     final remoteResults = state.resourceManager.resultManager.remoteResults;
     final filter = state.resourceManager.resultManager.resultFilter;
@@ -37,55 +40,13 @@ class _HistoryListScreenState extends State<HistoryListScreen> {
     List<ListItem> itemsList = _listItems(resultItems);
 
     return Scaffold(
-      appBar: AppBar(title: Text(l10n.menuHistory), actions: [
-        PopupMenuButton<SortByEnum>(
-          constraints: BoxConstraints(
-            maxWidth: MediaQuery.of(context).size.width,
-          ),
-          initialValue: sort.sortBy,
-          onSelected: (SortByEnum item) {
-            setState(() {
-              sort.sortBy = item;
-            });
-          },
-          icon: const Icon(Icons.sort),
-          itemBuilder: (context) {
-            return [
-              PopupMenuItem<SortByEnum>(
-                value: SortByEnum.dateDesc,
-                child: Text(l10n.historySortByDateDesc),
-              ),
-              PopupMenuItem<SortByEnum>(
-                value: SortByEnum.dateAsc,
-                child: Text(l10n.historySortByDateAsc),
-              ),
-              const PopupMenuDivider(),
-              PopupMenuItem<SortByEnum>(
-                value: SortByEnum.taskThroughputDesc,
-                child: Text(l10n.historySortByTaskThroughputDesc),
-              ),
-              PopupMenuItem<SortByEnum>(
-                value: SortByEnum.taskThroughputAsc,
-                child: Text(l10n.historySortByTaskThroughputAsc),
-              ),
-            ];
-          },
-        ),
-        IconButton(
-          icon: Icon(filter.anyFilterActive
-              ? Icons.filter_list
-              : Icons.filter_list_off),
-          tooltip: l10n.historyFilterTitle,
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const ResultFilterScreen(),
-              ),
-            ).then((value) => setState(() {}));
-          },
-        )
-      ]),
+      appBar: AppBar(
+        title: Text(l10n.menuHistory),
+        actions: [
+          _sortButton(sort),
+          _filterButton(filter),
+        ],
+      ),
       body: ListView.separated(
         controller: ScrollController(),
         padding: const EdgeInsets.only(top: 20, bottom: 20),
@@ -96,6 +57,82 @@ class _HistoryListScreenState extends State<HistoryListScreen> {
           return item.build(context);
         },
       ),
+    );
+  }
+
+  PopupMenuButton<SortByEnum> _sortButton(ResultSort sort) {
+    return PopupMenuButton<SortByEnum>(
+      constraints: BoxConstraints(
+        maxWidth: MediaQuery.of(context).size.width,
+      ),
+      initialValue: sort.sortBy,
+      onSelected: (SortByEnum item) {
+        setState(() {
+          sort.sortBy = item;
+        });
+      },
+      icon: const Icon(Icons.sort),
+      itemBuilder: (context) {
+        return [
+          PopupMenuItem<SortByEnum>(
+            value: SortByEnum.dateDesc,
+            child: ListTile(
+              contentPadding: const EdgeInsets.fromLTRB(4, 0, 4, 0),
+              minLeadingWidth: 32,
+              leading: const Icon(Icons.date_range),
+              trailing: const Icon(Icons.south),
+              title: Text(l10n.historySortByDate),
+            ),
+          ),
+          PopupMenuItem<SortByEnum>(
+            value: SortByEnum.dateAsc,
+            child: ListTile(
+              contentPadding: const EdgeInsets.fromLTRB(4, 0, 4, 0),
+              minLeadingWidth: 32,
+              leading: const Icon(Icons.date_range),
+              trailing: const Icon(Icons.north),
+              title: Text(l10n.historySortByDate),
+            ),
+          ),
+          const PopupMenuDivider(),
+          PopupMenuItem<SortByEnum>(
+            value: SortByEnum.taskThroughputDesc,
+            child: ListTile(
+              contentPadding: const EdgeInsets.fromLTRB(4, 0, 4, 0),
+              minLeadingWidth: 32,
+              leading: const Icon(Icons.speed),
+              trailing: const Icon(Icons.south),
+              title: Text(l10n.historySortByTaskThroughput),
+            ),
+          ),
+          PopupMenuItem<SortByEnum>(
+            value: SortByEnum.taskThroughputAsc,
+            child: ListTile(
+              contentPadding: const EdgeInsets.fromLTRB(4, 0, 4, 0),
+              minLeadingWidth: 32,
+              leading: const Icon(Icons.speed),
+              trailing: const Icon(Icons.north),
+              title: Text(l10n.historySortByTaskThroughput),
+            ),
+          ),
+        ];
+      },
+    );
+  }
+
+  Widget _filterButton(ResultFilter filter) {
+    return IconButton(
+      icon: Icon(
+          filter.anyFilterActive ? Icons.filter_list : Icons.filter_list_off),
+      tooltip: l10n.historyFilterTitle,
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ResultFilterScreen(),
+          ),
+        ).then((value) => setState(() {}));
+      },
     );
   }
 
