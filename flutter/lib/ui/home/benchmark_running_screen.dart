a8f9b0413eb5f20228409902a724b6f9aad04e3a
Anh <anh.app.dev@gmail.com>
2024-01-19 15:55:05 +0700

    Show task list

--- flutter/lib/ui/home/benchmark_running_screen.dart
+++ flutter/lib/ui/home/benchmark_running_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:mlperfbench/benchmark/benchmark.dart';
 
 import 'package:mlperfbench/state/task_runner.dart';
 import 'package:provider/provider.dart';
@@ -25,12 +27,32 @@ class _BenchmarkRunningScreenState extends State<BenchmarkRunningScreen> {
   late final Timer _timer;
   late BenchmarkState state;
   late AppLocalizations l10n;
+  late ProgressInfo progress;
+
+  @override
+  void initState() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) => setState(() {}));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     state = context.watch<BenchmarkState>();
     l10n = AppLocalizations.of(context);
-    final progress = state.taskRunner.progressInfo;
+    progress = state.taskRunner.progressInfo;
+
+    // TODO: to delete mockup
+    progress.info = state.benchmarks.last.info;
+    for (var e in state.benchmarks) {
+      e.isActive = true;
+    }
+    // END
 
     final backgroundGradient = BoxDecoration(
       gradient: LinearGradient(
@@ -39,10 +61,6 @@ class _BenchmarkRunningScreenState extends State<BenchmarkRunningScreen> {
         end: Alignment.bottomCenter,
       ),
     );
-    final title = _title();
-    final circle = _circle(progress);
-    final namedIcon = _namedIcon(progress);
-    final cancelButton = _cancelButton();
 
     return Scaffold(
       key: BenchmarkRunningScreen.scaffoldKey,
@@ -52,13 +70,208 @@ class _BenchmarkRunningScreenState extends State<BenchmarkRunningScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            title,
-            circle,
-            namedIcon,
-            cancelButton,
+            Expanded(flex: 14, child: _title()),
+            Expanded(flex: 30, child: _circle()),
+            Expanded(flex: 40, child: _taskList()),
+            Expanded(flex: 16, child: _footer()),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _title() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(40, 40, 40, 4),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            l10n.progressMeasuring,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              color: AppColors.lightText,
+              fontSize: 30,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _circle() {
+    final containerWidth = 0.64 * MediaQuery.of(context).size.width;
+    final progressCircleSize = Size(containerWidth + 60, containerWidth + 60);
+    return Stack(
+      alignment: AlignmentDirectional.centerStart,
+      children: <Widget>[
+        Center(
+          child: Container(
+              width: containerWidth,
+              height: containerWidth,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  colors: AppColors.progressCircleGradient,
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(15, 15),
+                    blurRadius: 10,
+                  )
+                ],
+              ),
+              child: Center(child: _circleContent())
+              // child: Text(
+              //   '${progress.currentStage.toString()}/${progress.totalStages.toString()}',
+              //   style: const TextStyle(
+              //     fontWeight: FontWeight.bold,
+              //     color: AppColors.lightText,
+              //   ),
+              //   textScaleFactor: 3,
+              // ),
+              // ),
+              ),
+        ),
+        Center(child: ProgressCircles(progressCircleSize))
+      ],
+    );
+  }
+
+  Widget _circleContent() {
+    Widget? taskIcon;
+    String progressString;
+    String taskNameString;
+    if (progress.cooldown) {
+      taskIcon = null;
+      progressString = l10n.progressScreenCooldown.replaceAll(
+          '<remaining>',
+          formatDuration(
+              progress.cooldownDuration * (1.0 - progress.stageProgress)));
+      taskNameString = l10n.progressCooldown;
+    } else {
+      progressString =
+          '${(progress.stageProgress * 100).round().clamp(0, 100)}%';
+      taskNameString = progress.info!.taskName;
+      taskIcon = progress.info!.iconWhite;
+    }
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 40, 20, 10),
+          child: SizedBox(
+            width: 32,
+            height: 32,
+            child: taskIcon,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+          child: Text(
+            progressString,
+            style: const TextStyle(
+              fontSize: 60,
+              fontWeight: FontWeight.bold,
+              color: AppColors.lightText,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 0, 20, 40),
+          child: Text(
+            taskNameString,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              color: AppColors.lightText,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _taskList() {
+    final childrenList = <Widget>[];
+    state.benchmarks.where((e) => e.isActive).forEachIndexed((idx, benchmark) {
+      childrenList.add(_listTile(benchmark, idx % 2 == 0));
+    });
+    return Material(
+      type: MaterialType.transparency,
+      child: ListView(
+        shrinkWrap: true,
+        children: childrenList,
+      ),
+    );
+  }
+
+  Widget _listTile(Benchmark benchmark, bool isEven) {
+    final leadingWidth = 0.08 * MediaQuery.of(context).size.width;
+    final titleWidth = 0.70 * MediaQuery.of(context).size.width;
+    final trailingWidth = 0.12 * MediaQuery.of(context).size.width;
+    return ListTile(
+      tileColor: isEven ? AppColors.mediumBlue : Colors.transparent,
+      textColor: AppColors.lightText,
+      dense: true,
+      minVerticalPadding: 0,
+      leading: SizedBox(
+          width: leadingWidth,
+          height: leadingWidth,
+          child: benchmark.info.iconWhite),
+      title: SizedBox(
+        width: titleWidth,
+        child: Text(
+          benchmark.info.taskName,
+          style: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      trailing: SizedBox(
+        width: trailingWidth,
+        height: trailingWidth,
+        child: const Icon(Icons.done),
+      ),
+    );
+  }
+
+  Widget _footer() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _footerText(),
+        _cancelButton(),
+      ],
+    );
+  }
+
+  Widget _footerText() {
+    return Wrap(
+      crossAxisAlignment: WrapCrossAlignment.center,
+      children: [
+        const Icon(
+          Icons.warning,
+          color: AppColors.lightText,
+        ),
+        const SizedBox(width: 8),
+        Text(
+          l10n.progressDontClose,
+          style: const TextStyle(
+            color: AppColors.lightText,
+            fontSize: 17,
+          ),
+        )
+      ],
     );
   }
 
@@ -89,131 +302,5 @@ class _BenchmarkRunningScreenState extends State<BenchmarkRunningScreen> {
         ),
       ),
     );
-  }
-
-  Widget _namedIcon(ProgressInfo progress) {
-    return Column(children: [
-      Padding(
-        padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
-        child: SizedBox(
-          width: 100,
-          height: 100,
-          child: progress.cooldown ? null : progress.info!.iconWhite,
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-        child: Text(
-          progress.cooldown
-              ? l10n.progressCooldown
-              : (progress.accuracy
-                      ? l10n.progressScreenNameAccuracy
-                      : l10n.progressScreenNamePerformance)
-                  .replaceFirst('<taskName>', progress.info!.taskName),
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
-            color: AppColors.lightText,
-          ),
-        ),
-      ),
-      Text(
-        progress.cooldown
-            ? l10n.progressScreenCooldown.replaceAll(
-                '<remaining>',
-                formatDuration(
-                    progress.cooldownDuration * (1.0 - progress.stageProgress)))
-            : l10n.progressScreenStage.replaceFirst(
-                '<percent>',
-                (progress.stageProgress * 100)
-                    .round()
-                    .clamp(0, 100)
-                    .toString()),
-        style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: AppColors.lightText,
-        ),
-      ),
-    ]);
-  }
-
-  Widget _circle(ProgressInfo progress) {
-    final containerWidth = 0.64 * MediaQuery.of(context).size.width;
-    final progressCircleSize = Size(containerWidth + 40, containerWidth + 40);
-    return Stack(
-      alignment: AlignmentDirectional.centerStart,
-      children: <Widget>[
-        Center(
-          child: Container(
-            width: containerWidth,
-            height: containerWidth,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: LinearGradient(
-                colors: AppColors.progressCircleGradient,
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black12,
-                  offset: Offset(15, 15),
-                  blurRadius: 10,
-                )
-              ],
-            ),
-            child: Center(
-              child: Text(
-                '${progress.currentStage.toString()}/${progress.totalStages.toString()}',
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: AppColors.lightText,
-                ),
-                textScaleFactor: 3,
-              ),
-            ),
-          ),
-        ),
-        Center(child: ProgressCircles(progressCircleSize))
-      ],
-    );
-  }
-
-  Widget _title() {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(40, 80, 20, 40),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            l10n.progressMeasuring,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              color: AppColors.lightText,
-              fontSize: 30,
-            ),
-          ),
-          Text(
-            l10n.progressDontClose,
-            style: const TextStyle(
-              color: AppColors.lightText,
-              fontSize: 17,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  void initState() {
-    _timer = Timer.periodic(const Duration(seconds: 1), (_) => setState(() {}));
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
   }
 }
