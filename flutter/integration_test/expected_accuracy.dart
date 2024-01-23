f7fee62bb261d760bbd30560a6e6e60879ed1646
Anh <anh.app.dev@gmail.com>
2024-01-23 23:32:20 +0700

    Add expected accuracy and throughput for QTI

--- flutter/integration_test/expected_accuracy.dart
+++ flutter/integration_test/expected_accuracy.dart
@@ -9,7 +9,9 @@ key: <accelerator> OR <accelerator>|<backendName>
 - npu -> Android TFLite
 - tpu -> Android Pixel
 - ane -> iOS TFLite or Core ML
-- cpu&gpu&ane -> IOS Core ML
+- cpu&gpu&ane -> iOS Core ML
+- snpe_dsp -> Android QTI
+- psnpe_dsp -> Android QTI
 */
 
 const Map<String, Interval> _imageClassification = {
@@ -17,7 +19,9 @@ const Map<String, Interval> _imageClassification = {
   'npu': Interval(min: 0.89, max: 0.91),
   'tpu': Interval(min: 0.89, max: 0.91),
   'ane': Interval(min: 1.00, max: 1.00),
-  'cpu&gpu&ane': Interval(min: 1.00, max: 1.00)
+  'cpu&gpu&ane': Interval(min: 1.00, max: 1.00),
+  'snpe_dsp': Interval(min: 0.78, max: 0.82),
+  'psnpe_dsp': Interval(min: 0.78, max: 0.82),
 };
 
 const Map<String, Interval> _objectDetection = {
@@ -26,7 +30,9 @@ const Map<String, Interval> _objectDetection = {
   'tpu': Interval(min: 0.36, max: 0.38),
   'ane|TFLite': Interval(min: 0.31, max: 0.34),
   'ane|Core ML': Interval(min: 0.45, max: 0.46),
-  'cpu&gpu&ane': Interval(min: 0.45, max: 0.46)
+  'cpu&gpu&ane': Interval(min: 0.45, max: 0.46),
+  'snpe_dsp': Interval(min: 0.32, max: 0.35),
+  'psnpe_dsp': Interval(min: 0.32, max: 0.35),
 };
 
 const Map<String, Interval> _imageSegmentation = {
@@ -35,7 +41,9 @@ const Map<String, Interval> _imageSegmentation = {
   'tpu': Interval(min: 0.33, max: 0.34),
   'ane|TFLite': Interval(min: 0.38, max: 0.40),
   'ane|Core ML': Interval(min: 0.38, max: 0.40),
-  'cpu&gpu&ane': Interval(min: 0.38, max: 0.40)
+  'cpu&gpu&ane': Interval(min: 0.38, max: 0.40),
+  'snpe_dsp': Interval(min: 0.35, max: 0.38),
+  'psnpe_dsp': Interval(min: 0.35, max: 0.38),
 };
 
 const Map<String, Interval> _naturalLanguageProcessing = {
@@ -44,7 +52,9 @@ const Map<String, Interval> _naturalLanguageProcessing = {
   'gpu|TFLite': Interval(min: 1.00, max: 1.00),
   // 1.00 in simulator, 0.80 on iphone 12 mini
   'gpu|Core ML': Interval(min: 0.80, max: 1.00),
-  'cpu&gpu&ane': Interval(min: 0.80, max: 1.00)
+  'cpu&gpu&ane': Interval(min: 0.80, max: 1.00),
+  'snpe_dsp': Interval(min: 1.00, max: 1.00),
+  'psnpe_dsp': Interval(min: 1.00, max: 1.00),
 };
 
 const Map<String, Interval> _superResolution = {
@@ -53,7 +63,9 @@ const Map<String, Interval> _superResolution = {
   'tpu': Interval(min: 0.32, max: 0.35),
   'ane|TFLite': Interval(min: 0.32, max: 0.35),
   'ane|Core ML': Interval(min: 0.32, max: 0.35),
-  'cpu&gpu&ane': Interval(min: 0.32, max: 0.35)
+  'cpu&gpu&ane': Interval(min: 0.32, max: 0.35),
+  'snpe_dsp': Interval(min: 0.32, max: 0.35),
+  'psnpe_dsp': Interval(min: 0.32, max: 0.35),
 };
 
 const benchmarkExpectedAccuracy = {
