e2404411bbaa7c5e2e93f8e013bda53d7e894f6c
Anh <anh.app.dev@gmail.com>
2024-08-21 00:28:08 +0700

    Add init_pipeline()

--- mobile_back_tflite/cpp/backend_tflite/tflite_c.cc
+++ mobile_back_tflite/cpp/backend_tflite/tflite_c.cc
@@ -34,7 +34,19 @@ std::string GetPlatformName();
 extern "C" {
 #endif  // __cplusplus
 
-auto pipeline = SingleModelPipeline();
+std::unique_ptr<Pipeline> pipeline;
+
+void init_pipeline(bool sd_pipeline) {
+  if (sd_pipeline) {
+    LOG(INFO) << "Initializing StableDiffusionPipeline";
+    pipeline = std::make_unique<StableDiffusionPipeline>();
+  } else {
+    LOG(INFO) << "Initializing SingleModelPipeline";
+    pipeline = std::make_unique<SingleModelPipeline>();
+  }
+}
+
+void reset_pipeline() { pipeline.reset(); }
 
 #if defined(__ANDROID__) && defined(MTK_TFLITE_NEURON_BACKEND)
 static bool neuron_tflite_backend(const char **not_allowed_message,
@@ -132,87 +144,89 @@ bool mlperf_backend_matches_hardware(const char **not_allowed_message,
 mlperf_backend_ptr_t mlperf_backend_create(
     const char *model_path, mlperf_backend_configuration_t *configs,
     const char *native_lib_path) {
-  return pipeline.backend_create(model_path, configs, native_lib_path);
+  init_pipeline(false);
+  return pipeline->backend_create(model_path, configs, native_lib_path);
 }
 
 // Vendor name who create this backend.
 const char *mlperf_backend_vendor_name(mlperf_backend_ptr_t backend_ptr) {
-  return pipeline.backend_vendor_name(backend_ptr);
+  return pipeline->backend_vendor_name(backend_ptr);
 }
 
 // TODO: Return the name of the accelerator.
 const char *mlperf_backend_accelerator_name(mlperf_backend_ptr_t backend_ptr) {
-  return pipeline.backend_accelerator_name(backend_ptr);
+  return pipeline->backend_accelerator_name(backend_ptr);
 }
 
 // Return the name of this backend.
 const char *mlperf_backend_name(mlperf_backend_ptr_t backend_ptr) {
-  return pipeline.backend_name(backend_ptr);
+  return pipeline->backend_name(backend_ptr);
 }
 
 // Destroy the backend pointer and its data.
 void mlperf_backend_delete(mlperf_backend_ptr_t backend_ptr) {
-  pipeline.backend_delete(backend_ptr);
+  pipeline->backend_delete(backend_ptr);
+  reset_pipeline();
 }
 
 // Run the inference for a sample.
 mlperf_status_t mlperf_backend_issue_query(mlperf_backend_ptr_t backend_ptr) {
-  return pipeline.backend_issue_query(backend_ptr);
+  return pipeline->backend_issue_query(backend_ptr);
 }
 
 // Flush the staged queries immediately.
 mlperf_status_t mlperf_backend_flush_queries(mlperf_backend_ptr_t backend_ptr) {
-  return pipeline.backend_flush_queries(backend_ptr);
+  return pipeline->backend_flush_queries(backend_ptr);
 }
 
 // Return the number of inputs of the model.
 int32_t mlperf_backend_get_input_count(mlperf_backend_ptr_t backend_ptr) {
-  return pipeline.backend_get_input_count(backend_ptr);
+  return pipeline->backend_get_input_count(backend_ptr);
 }
 
 // Return the type of the ith input.
 mlperf_data_t mlperf_backend_get_input_type(mlperf_backend_ptr_t backend_ptr,
                                             int32_t i) {
-  return pipeline.backend_get_input_type(backend_ptr, i);
+  return pipeline->backend_get_input_type(backend_ptr, i);
 }
 
 // Set the data for ith input.
 mlperf_status_t mlperf_backend_set_input(mlperf_backend_ptr_t backend_ptr,
                                          int32_t batch_index, int32_t i,
                                          void *data) {
-  return pipeline.backend_set_input(backend_ptr, batch_index, i, data);
+  return pipeline->backend_set_input(backend_ptr, batch_index, i, data);
 }
 
 // Return the number of outputs for the model.
 int32_t mlperf_backend_get_output_count(mlperf_backend_ptr_t backend_ptr) {
-  return pipeline.backend_get_output_count(backend_ptr);
+  return pipeline->backend_get_output_count(backend_ptr);
 }
 
 // Return the type of ith output.
 mlperf_data_t mlperf_backend_get_output_type(mlperf_backend_ptr_t backend_ptr,
                                              int32_t i) {
-  return pipeline.backend_get_output_type(backend_ptr, i);
+  return pipeline->backend_get_output_type(backend_ptr, i);
 }
 
 // Get the data from ith output.
 mlperf_status_t mlperf_backend_get_output(mlperf_backend_ptr_t backend_ptr,
                                           uint32_t batch_index, int32_t i,
                                           void **data) {
-  return pipeline.backend_get_output(backend_ptr, batch_index, i, data);
+  return pipeline->backend_get_output(backend_ptr, batch_index, i, data);
 }
 
 void mlperf_backend_convert_inputs(mlperf_backend_ptr_t backend_ptr, int bytes,
                                    int width, int height, uint8_t *data) {
-  return pipeline.backend_convert_inputs(backend_ptr, bytes, width, height,
-                                         data);
+  return pipeline->backend_convert_inputs(backend_ptr, bytes, width, height,
+                                          data);
 }
 
 void *mlperf_backend_get_buffer(size_t n) {
-  return pipeline.backend_get_buffer(n);
+  return pipeline->backend_get_buffer(n);
 }
 
 void mlperf_backend_release_buffer(void *p) {
-  return pipeline.backend_release_buffer(p);
+  return pipeline->backend_release_buffer(p);
 }
 
 #ifdef __cplusplus
