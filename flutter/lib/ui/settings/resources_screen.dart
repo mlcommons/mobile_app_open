d56a5309a441d060b0daef96ee0c5708276a8af5
Anh <anh.app.dev@gmail.com>
2024-11-13 18:56:26 +0700

    Show download progress and url

--- flutter/lib/ui/settings/resources_screen.dart
+++ flutter/lib/ui/settings/resources_screen.dart
@@ -1,23 +1,14 @@
-import 'dart:io';
-
 import 'package:bot_toast/bot_toast.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mlperfbench/benchmark/benchmark.dart';
 
 import 'package:provider/provider.dart';
 
-import 'package:mlperfbench/app_constants.dart';
 import 'package:mlperfbench/benchmark/run_mode.dart';
 import 'package:mlperfbench/benchmark/state.dart';
-import 'package:mlperfbench/build_info.dart';
-import 'package:mlperfbench/firebase/firebase_manager.dart';
 import 'package:mlperfbench/localizations/app_localizations.dart';
-import 'package:mlperfbench/resources/config_manager.dart';
 import 'package:mlperfbench/store.dart';
-import 'package:mlperfbench/ui/app_styles.dart';
 import 'package:mlperfbench/ui/confirm_dialog.dart';
-import 'package:mlperfbench/ui/settings/task_config_section.dart';
 
 class ResourcesScreen extends StatefulWidget {
   const ResourcesScreen({super.key});
@@ -31,6 +22,9 @@ class _ResourcesScreen extends State<ResourcesScreen> {
   late BenchmarkState state;
   late Store store;
 
+  bool get downloading =>
+      (state.loadingProgress > 0.0 && state.loadingProgress < 0.999);
+
   @override
   Widget build(BuildContext context) {
     store = context.watch<Store>();
@@ -43,8 +37,10 @@ class _ResourcesScreen extends State<ResourcesScreen> {
       children.add(_listTileBuilder(benchmark));
       children.add(const Divider(height: 20));
     }
+    children.add(const SizedBox(height: 20));
+    children.add(_downloadProgress());
     children.add(_downloadButton());
-    children.add(const Divider(height: 20));
+    children.add(const SizedBox(height: 20));
     children.add(_clearCacheButton());
 
     return Scaffold(
@@ -105,47 +101,85 @@ class _ResourcesScreen extends State<ResourcesScreen> {
             ],
           );
         } else {
-          return const Text('Checking download status');
+          return Text(l10n.resourceChecking);
         }
       },
     );
   }
 
-  Widget _downloadButton() {
-    return ElevatedButton(
-      onPressed: () {
-        print('download resources');
-      },
-      child: const FittedBox(
-        fit: BoxFit.scaleDown,
-        child: Text('Download'),
+  Widget _downloadProgress() {
+    if (!downloading) {
+      return const SizedBox(height: 0);
+    }
+    return SizedBox(
+      height: 88,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            state.loadingPath,
+            maxLines: 5,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              fontSize: 12.0,
+            ),
+          ),
+          const SizedBox(height: 8),
+          LinearProgressIndicator(
+            value: state.loadingProgress,
+            minHeight: 8,
+          ),
+        ],
       ),
     );
   }
 
+  Widget _downloadButton() {
+    return AbsorbPointer(
+      absorbing: downloading,
+      child: ElevatedButton(
+        onPressed: () {
+          state.loadResources(downloadMissing: true);
+        },
+        style: ElevatedButton.styleFrom(
+            backgroundColor: downloading ? Colors.grey : Colors.blue),
+        child: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Text(l10n.resourceDownload),
+        ),
+      ),
+    );
+  }
+
+  // TODO: add confirm dialog
   Widget _clearCacheButton() {
-    return ElevatedButton(
-      onPressed: () async {
-        final dialogAction =
-            await showConfirmDialog(context, l10n.settingsClearCacheConfirm);
-        switch (dialogAction) {
-          case ConfirmDialogAction.ok:
-            await state.clearCache();
-            BotToast.showSimpleNotification(
-              title: l10n.settingsClearCacheFinished,
-              hideCloseButton: true,
-            );
-            break;
-          case ConfirmDialogAction.cancel:
-            break;
-          default:
-            break;
-        }
-      },
-      style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-      child: FittedBox(
-        fit: BoxFit.scaleDown,
-        child: Text(l10n.settingsClearCache),
+    return AbsorbPointer(
+      absorbing: downloading,
+      child: ElevatedButton(
+        onPressed: () async {
+          final dialogAction =
+              await showConfirmDialog(context, l10n.settingsClearCacheConfirm);
+          switch (dialogAction) {
+            case ConfirmDialogAction.ok:
+              await state.clearCache();
+              BotToast.showSimpleNotification(
+                title: l10n.settingsClearCacheFinished,
+                hideCloseButton: true,
+              );
+              break;
+            case ConfirmDialogAction.cancel:
+              break;
+            default:
+              break;
+          }
+        },
+        style: ElevatedButton.styleFrom(
+            backgroundColor: downloading ? Colors.grey : Colors.red),
+        child: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Text(l10n.resourceClear),
+        ),
       ),
     );
   }
