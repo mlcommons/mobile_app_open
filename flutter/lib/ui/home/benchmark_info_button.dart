a2485811441ddeff38693ab3d729873badd90799
Farook (EDev) Al-Sammarraie <farook.a@scopicsoftware.com>
2025-03-12 15:49:08 +0000

    fix: made task descriptions scrollable + task title overflow fix (#980)

--- flutter/lib/ui/home/benchmark_info_button.dart
+++ flutter/lib/ui/home/benchmark_info_button.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:mlperfbench/benchmark/benchmark.dart';
 import 'package:mlperfbench/localizations/app_localizations.dart';
+import 'package:mlperfbench/ui/auto_size_text.dart';
+import 'package:mlperfbench/ui/home/app_drawer.dart';
 
 class BenchmarkInfoButton extends StatelessWidget {
   final Benchmark benchmark;
@@ -23,6 +25,10 @@ class BenchmarkInfoButton extends StatelessWidget {
 
     final info = benchmark.info.getLocalizedInfo(l10n);
 
+    const double sidePadding = 18.0;
+    const double headHeight = 48.0 + (18.0 * 2);
+    const double footHeight = 36.0;
+
     showModalBottomSheet(
       context: context,
       isDismissible: false,
@@ -30,53 +36,68 @@ class BenchmarkInfoButton extends StatelessWidget {
       isScrollControlled: true,
       shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.vertical(top: Radius.circular(24))),
-      builder: (context) => Wrap(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Flexible(
-                  flex: 5,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
+      builder: (context) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: sidePadding),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: headHeight,
+              width: MediaQuery.of(context).size.width - sidePadding,
+              child: Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: AutoSizeText(
                         benchmark.taskConfig.name,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1, //can be changed to 2 without issue
                         textAlign: TextAlign.left,
                         style: const TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 28,
                         ),
                       ),
-                    ],
-                  ),
-                ),
-                Flexible(
-                  flex: 1,
-                  child: Container(
-                    alignment: Alignment.topRight,
-                    child: IconButton(
+                    ),
+                    IconButton(
                       splashRadius: 24,
                       onPressed: () => Navigator.pop(context),
                       icon: const Icon(Icons.close, color: Colors.grey),
                     ),
+                  ],
+                ),
+              ),
+            ),
+            LayoutBuilder(builder: (context, constraints) {
+              print(constraints.maxHeight);
+              return ConstrainedBox(
+                constraints: constraints.copyWith(
+                    maxHeight: constraints.maxHeight != double.infinity
+                        ? constraints.maxHeight
+                        : MediaQuery.of(context).size.height - headHeight),
+                child: ScrollConfiguration(
+                  behavior: NoGlowScrollBehavior(),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Text(
+                          info.detailsContent,
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                        const SizedBox(
+                          height: footHeight,
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 20, 40),
-            child: Text(
-              info.detailsContent,
-              style: const TextStyle(fontSize: 16),
-            ),
-          ),
-        ],
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
