e693a22090f8e43c7ee378dd97593adbc370c6f6
anhappdev <anh.app.dev@gmail.com>
2025-10-06 22:48:10 +0700

    Add `--break-system-packages` flag and remove redundant pip upgrade in iOS CI post-clone script.

--- flutter/ios/ci_scripts/ci_post_clone.sh
+++ flutter/ios/ci_scripts/ci_post_clone.sh
@@ -65,8 +65,7 @@ brew install cocoapods || brew install cocoapods || brew install cocoapods
 echo "$MC_LOG_PREFIX cocoapods version:" && pod --version
 
 echo "$MC_LOG_PREFIX python version:" && python3 --version
-python3 -m pip install --upgrade pip
-python3 -m pip install \
+python3 -m pip install --break-system-packages \
   "numpy>=1.23,<2.0" \
   "absl-py>=1.3,<2.0"
 
