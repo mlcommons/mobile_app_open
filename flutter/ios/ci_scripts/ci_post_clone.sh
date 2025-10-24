6eed82c9ea01b0e027628a106aa464df4d22ab9e
anhappdev <anh.app.dev@gmail.com>
2025-10-24 17:07:17 +0700

    Add Python 3.14 installation in iOS CI post-clone script

--- flutter/ios/ci_scripts/ci_post_clone.sh
+++ flutter/ios/ci_scripts/ci_post_clone.sh
@@ -64,8 +64,9 @@ echo "$MC_LOG_PREFIX protobuf version:" && protoc --version
 brew install cocoapods || brew install cocoapods || brew install cocoapods
 echo "$MC_LOG_PREFIX cocoapods version:" && pod --version
 
+brew install python@3.14 || brew install python@3.14 || brew install python@3.14
 echo "$MC_LOG_PREFIX python version:" && python3 --version
-python3 -m pip install pip==25.1
+
 python3 -m pip install --break-system-packages \
   "numpy>=1.23,<2.0" \
   "absl-py>=1.3,<2.0"
