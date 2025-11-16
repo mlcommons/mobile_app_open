4c1dc852ccaffc5dc640a203414d8e17d805e55f
Farook Al-Sammarraie <farook.a@scopicsoftware.com>
2025-11-17 00:10:00 +0300

    fixed android build failing on macos

--- flutter/android/android.mk
+++ flutter/android/android.mk
@@ -57,6 +57,7 @@ flutter/android/libs/build:
 	bazel ${BAZEL_OUTPUT_ROOT_ARG} ${proxy_bazel_args} ${sonar_bazel_startup_options} \
 		build ${BAZEL_CACHE_ARG} ${bazel_links_arg} ${sonar_bazel_build_args} \
 		--config=android_arm64 \
+		--platforms=//platforms:android_arm64 \
 		${backend_tflite_android_target} \
 		${backend_mediatek_android_target} \
 		${backend_pixel_android_target} \
